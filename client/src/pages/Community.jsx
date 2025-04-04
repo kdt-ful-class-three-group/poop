f5ce61c878f042c059afebd6481f604f83483337
김윤지 <yunw0117@gmail.com>
2025-04-04 16:56:33 +0900

    react router

---
+++ client/src/pages/Community.jsx
@@ -0,0 +1,10 @@
+function Community() {
+  return (
+    <div className="container">
+      <h1>Community</h1>
+      <p>Welcome to the community page!</p>
+    </div>
+  );
+}
+
+export default Community;
---
+++ client/src/pages/Knowledge.jsx
@@ -0,0 +1,10 @@
+function Knowledge() {
+  return (
+    <div>
+      <h1>Knowledge</h1>
+      <p>This is the knowledge page.</p>
+    </div>
+  );
+}
+
+export default Knowledge;
