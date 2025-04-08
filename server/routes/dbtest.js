7ecc45f4915b0d74aeb947686a8220beed1a3ceb
윤종환 <yunjonghwan99@gmail.com>
2025-04-08 15:54:34 +0900

    []로 변수를 담을 경우 테이블의 정보를 가져오는 것이 아닌 select한 결과 값 즉 행의 값을 가져오게 됨

--- server/routes/dbtest.js
+++ server/routes/dbtest.js
@@ -4,8 +4,9 @@ import pool from '../config/database.js'
 const commentRouter = express.Router();
 
 commentRouter.get('/test', async (req, res) => {
-  const data = await pool.execute('SELECT * FROM comment');
+  const [data] = await pool.execute('SELECT * FROM comment');
   console.log(data);
+  res.json(data);
 })
 
 export default commentRouter;
