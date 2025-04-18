12ebde7f5a45eb24749ebe5d5f3e2f0c242403e2
muunji <twoji3019@naver.com>
2025-04-18 14:36:58 +0900

    작성

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
+//시작하기
+//왼쪽으로 넘겨주세요
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <p>This is the Home page.</p>
+            <h1>시작하기</h1>
+            <p>왼쪽으로 넘겨주세요</p>
         </div>
     );
 }
