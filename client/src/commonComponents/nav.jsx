e3940cce2946ff390b39001ae5016b707ffd76a0
muunji <twoji3019@naver.com>
2025-04-18 16:10:15 +0900

    버튼 생성해봄

--- client/src/commonComponents/nav.jsx
+++ client/src/commonComponents/nav.jsx
@@ -1,5 +1,25 @@
-function nav() {
+import { useNavigate } from "react-router"
 
+function Nav() {
+  const navigate = useNavigate()
+
+  const goToQuiz=()=>{
+    navigate("/Quiz")
+  }
+  const goToHorror = ()=>{
+    navigate("/Horror")
+  }
+  const goToKnowledge = ()=>{
+    navigate("/Knowledge")
+  }
+
+  return (
+    <div>
+      <button onClick={navigate("/Quiz")}>퀴즈</button>
+      <button onClick={goToHorror}>괴담</button>
+      <button onClick={goToKnowledge}>상식</button>
+    </div>
+  )
 }
 
-export default nav
+export default Nav
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import './home.css'
+import Nav from '../commonComponents/nav';
 
 function Home() {
     return (
         <div>
+          <Nav/>
             <h1 className="text-cyan-400">Home</h1>
             <p>This is the Home page.</p>
         </div>
