df7d0c3e0dccf1c6ccce00dd662ab2d14872e7cf
김윤지 <yunw0117@gmail.com>
2025-04-18 16:46:59 +0900

    dev pull하기 위한 커밋

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -1,5 +1,31 @@
-function Menu() {
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-}
+const Menu = ({ onSelect }) => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex gap-4">
+        <button
+          onClick={() => onSelect("/Quiz")}
+          className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-md shadow"
+        >
+          퀴즈
+        </button>
+        <button
+          onClick={() => onSelect("knowledge")}
+          className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-md shadow"
+        >
+          상식
+        </button>
+        <button
+          onClick={() => onSelect("horror")}
+          className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded-md shadow"
+        >
+          괴담
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
