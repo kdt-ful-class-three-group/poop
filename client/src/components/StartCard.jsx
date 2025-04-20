33931583825b8784edef071494abf94f6a9fc6bf
최정민 <bigdragon00@naver.com>
2025-04-21 00:06:27 +0900

    시작하기 카드 컴포넌트로 진행해봄 #8

---
+++ client/src/components/StartCard.jsx
@@ -0,0 +1,13 @@
+
+
+function StartCard(){
+    return (
+        <div className="w-[312px] h-[455px] border border-black flex flex-col justify-center items-center box-border">
+
+            <div className="text-2xl font-bold mb-4 text-red-500">시작하기</div>
+            <div className="text-base">왼쪽으로 넘겨주세요</div>
+        </div>
+    )
+}
+
+export default StartCard;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,16 @@
-import './home.css';
+
 import Menu from "../commonComponents/menu.jsx";
-
-
-
+import StartCard from "../components/StartCard.jsx";
 function Home() {
     return (
         <>
-            <div className="fixed width-full flex bg-whitep ">
-                <Menu/>
+            <Menu />
+
+
+            <div className="container mx-auto bg-white min-h-screen flex justify-center items-center">
+                <StartCard/>
             </div>
-            <div className=" flex w-full flex-col items-center justify-center h-screen">
-                <div className="text-center mt-24">
-                    <h1 className="text-cyan-600 text-3xl font-bold mb-4">Home</h1>
-                    <p className="text-gray-700 text-lg">This is the Home page.</p>
-                </div>
-            </div>
+
         </>
     );
 }
