c09e735eab78c948b1a21f2cf6943b56698a558c
최정민 <bigdragon00@naver.com>
2025-04-20 22:45:42 +0900

    메뉴 상단 고정

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,15 +5,17 @@ import Menu from "../commonComponents/menu.jsx";
 
 function Home() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <>
             <div className="fixed width-full flex bg-whitep ">
                 <Menu/>
             </div>
-            <div className="flex flex-col items-center justify-center h-screen">
-                <h1 className="text-cyan-600">Home</h1>
-                <p>This is the Home page.</p>
+            <div className=" flex w-full flex-col items-center justify-center h-screen">
+                <div className="text-center mt-24">
+                    <h1 className="text-cyan-600 text-3xl font-bold mb-4">Home</h1>
+                    <p className="text-gray-700 text-lg">This is the Home page.</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
