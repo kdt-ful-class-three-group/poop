f7462de153728424f0f8bcb12e1d16b327df8ba8
최정민 <bigdragon00@naver.com>
2025-04-20 22:41:40 +0900

    메뉴 상단 중앙 고정

--- client/src/commonComponents/menu.jsx
+++ client/src/commonComponents/menu.jsx
@@ -1,5 +1,27 @@
-function menu() {
+import {useNavigate} from 'react-router'
+
+function Menu() {
+    const navigate = useNavigate()
+
+
+    const goToQuiz = ()=>{
+        navigate("/Quiz")
+    }
+    const goToHorror = ()=>{
+        navigate("/Horror")
+    }
+    const goToKnowledge=()=>{
+        navigate("/Knowledge")
+    }
+
+    return(
+        <div className="fixed w-full flex top-10 left-0 bg-transparent p-4 justify-around">
+            <button onClick={goToQuiz}>퀴즈</button>
+            <button onClick={goToHorror}>괴담</button>
+            <button onClick={goToKnowledge}>상식</button>
+        </div>
+    )
 
 }
 
-export default menu
+export default Menu;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import './home.css';
+import Menu from "../commonComponents/menu.jsx";
+
+
+
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>This is the Home page.</p>
+        <div className="flex items-center justify-center h-screen">
+            <div className="fixed width-full flex bg-whitep ">
+                <Menu/>
+            </div>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <h1 className="text-cyan-600">Home</h1>
+                <p>This is the Home page.</p>
+            </div>
         </div>
     );
 }
