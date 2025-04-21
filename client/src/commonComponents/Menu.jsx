4509ceaf03a4d074e79b8b118857bb9b27826004
muunji <twoji3019@naver.com>
2025-04-21 11:31:07 +0900

    버튼 클릭하면 상태값 전달 > quiz에서 이동이 안됨

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -1,25 +1,35 @@
 import {useNavigate} from 'react-router'
 
-function Menu() {
+function Menu({setCategory}) {
   const navigate = useNavigate()
 
   //각각 페이지로 이동하는 함수 작성
   const goToQuiz = ()=>{
     navigate("/Quiz")
   }
-  // const goToHorror = ()=>{
-  //   navigate("/Horror")
-  // }
-  // const goToKnowledge=()=>{
-  //   navigate("/Knowledge")
-  // }
+  const goToHorror = ()=>{
+    if(setCategory){
+      setCategory('Horror')
+      return
+    } else {
+      navigate("/KnowledgeHorror")
+    }
+  }
+  const goToKnowledge=()=>{
+    if(setCategory){
+      setCategory('knowledge')
+      return
+    } else {
+      navigate("/KnowledgeHorror")
+    }
+  }
 
   //버튼 3개가 담긴 div 렌더
   return(
     <div>
       <button onClick={goToQuiz} className='cursor-pointer'>퀴즈</button>
-      <button className='cursor-pointer'>괴담</button>
-      <button className='cursor-pointer'>상식</button>
+      <button onClick={()=>setCategory('Horror')}className='cursor-pointer'>괴담</button>
+      <button onClick={()=>setCategory('Knowledge')}className='cursor-pointer'>상식</button>
     </div>
   )
   
