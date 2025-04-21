fb82a5756ec05c778e3c6f48c6f5f4d92896a524
윤종환 <yunjonghwan99@gmail.com>
2025-04-21 15:03:43 +0900

    navigate에서 상태값을 같이 넘김

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -4,25 +4,22 @@ function Menu() {
   const navigate = useNavigate();
 
   const quiz = () => {
-    console.log('quiz');
     navigate('/Quiz');
   }
 
-  const common_sense = () => {
-    console.log('common_sense');
-    navigate('/KnowledgeHorror');
-  }
-
-  const horror = () => {
-    console.log('horror');
-    navigate('/KnowledgeHorror');
+  const KnowledgeHorror = (category) => {
+    navigate('/KnowledgeHorror', {
+      state : {
+        category : category
+      }
+    });
   }
 
   return (
     <div className="border-1 flex justify-around">
       <button className="" onClick={() => quiz()}>퀴즈</button>
-      <button className="" onClick={() => common_sense()}>상식</button>
-      <button className="" onClick={() => horror()}>괴담</button>
+      <button className="" onClick={() => KnowledgeHorror('knowledge')}>상식</button>
+      <button className="" onClick={() => KnowledgeHorror('horror')}>괴담</button>
     </div>
   )
 }
