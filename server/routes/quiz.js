b375411dd229bc1a432a090c3aa0571a55ea9564
윤종환 <yunjonghwan99@gmail.com>
2025-04-21 11:30:54 +0900

    js로 인해서 database를 import 해오지 않는 에러 수정

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -1,5 +1,5 @@
 import express from "express";
-import pool from "../config/database";
+import pool from "../config/database.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
