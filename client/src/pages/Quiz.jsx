5493ae671b7933165219bce72cec622229441a57
윤종환 <yunjonghwan99@gmail.com>
2025-04-21 12:36:00 +0900

    Menu import

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -3,6 +3,7 @@ import Menu from '../commonComponents/Menu.jsx';
 function Quiz() {
     return (
         <div>
+            <Menu/>
             <h1>Quiz Page</h1>
             <p>This is the quiz page.</p>
         </div>
