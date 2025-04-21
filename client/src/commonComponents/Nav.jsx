e2119e9b30fa3b3ab44c04d8befaad4e91fde043
muunji <twoji3019@naver.com>
2025-04-21 12:38:54 +0900

    useState 사용

--- client/src/commonComponents/Nav.jsx
+++ client/src/commonComponents/Nav.jsx
@@ -1,7 +1,12 @@
+import {useEffect, useState} from 'react'
+
 function Nav() {
-  
+
+  const [usualNav, setUsualNav]= useState("flex justify-center align-bottom absolute w-full bottom-[20px]")
+  const [isClicked, setIsClicked] = useState(false)
+
   return(
-    <div className="flex justify-center align-bottom absolute w-full bottom-[20px]">
+    <div className={usualNav}>
       <div className="bg-gray-400 w-[70px] h-[70px] rounded-full cursor-pointer"></div>
     </div>
   )
