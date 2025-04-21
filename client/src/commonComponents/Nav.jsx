e9a71806f2add95d2d85a85c8cd671af4db55243
muunji <twoji3019@naver.com>
2025-04-21 12:37:50 +0900

    기본 스타일 적용

--- client/src/commonComponents/Nav.jsx
+++ client/src/commonComponents/Nav.jsx
@@ -1,5 +1,10 @@
 function Nav() {
-
+  
+  return(
+    <div className="flex justify-center align-bottom absolute w-full bottom-[20px]">
+      <div className="bg-gray-400 w-[70px] h-[70px] rounded-full cursor-pointer"></div>
+    </div>
+  )
 }
 
 export default Nav;
