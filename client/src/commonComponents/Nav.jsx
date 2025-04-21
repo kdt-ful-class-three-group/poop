2624ad595d5c6a45d17ee6bd0a99f0371f1f399b
muunji <twoji3019@naver.com>
2025-04-21 13:07:42 +0900

    펼쳤을 때 예시

--- client/src/commonComponents/Nav.jsx
+++ client/src/commonComponents/Nav.jsx
@@ -2,12 +2,15 @@ import {useEffect, useState} from 'react'
 
 function Nav() {
 
-  const [usualNav, setUsualNav]= useState("flex justify-center align-bottom absolute w-full bottom-[20px]")
-  const [isClicked, setIsClicked] = useState(false)
+  // const [usualNav, setUsualNav]= useState("flex justify-center align-bottom absolute w-full bottom-[20px]")
+  // const [isClicked, setIsClicked] = useState(false)
 
   return(
-    <div className={usualNav}>
-      <div className="bg-gray-400 w-[70px] h-[70px] rounded-full cursor-pointer"></div>
+    <div className='fixed bottom-[-200px] left-1/2 transform -translate-x-1/2 w-[450px] h-[450px] bg-gray-200 rounded-full'>
+      <div className='absolute w-[70px] h-[70px] bg-gray-400 bottom-1/2 left-[20px]'>1</div>
+      <div className='absolute w-[70px] h-[70px] bg-gray-400 left-1/2 transform -translate-x-1/2 top-[20px]'>2</div>
+      <div className='absolute w-[70px] h-[70px] bg-gray-400 bottom-1/2 right-[20px]'>3</div>
+      <div className='absolute w-[70px] h-[70px] bg-gray-400 bottom-1/2 left-1/2 transform -translate-x-1/2'>4</div>
     </div>
   )
 }
