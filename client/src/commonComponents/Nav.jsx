90b12a15279e987072fdd3e913844a0fda12b7a3
muunji <twoji3019@naver.com>
2025-04-21 15:08:50 +0900

    useNavigate로 클릭하면 이동

--- client/src/commonComponents/Nav.jsx
+++ client/src/commonComponents/Nav.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 function Nav() {
   const [open, setOpen] = useState(false);
+
+  const navigate = useNavigate()
 
   return(
     <div
@@ -13,21 +16,21 @@ function Nav() {
       {/* 버튼 1 */}
       <div className={`absolute w-[70px] h-[70px] bg-gray-400 bottom-1/2 left-[20px] transition-all duration-500 ease-in-out cursor-pointer rounded-full flex items-center justify-center ${
         open ? 'opacity-100' : 'opacity-0'
-      }`}>
+      }`} onClick={()=>navigate('/Quiz')}>
         유머
       </div>
       
       {/* 버튼 2 */}
       <div className={`absolute w-[70px] h-[70px] bg-gray-400 left-1/2 transform -translate-x-1/2 top-[20px] transition-all duration-500 ease-in-out cursor-pointer rounded-full flex items-center justify-center ${
         open ? 'opacity-100' : 'opacity-0'
-      }`}>
+      }`} onClick={()=>navigate('/Community')}>
         커뮤니티
       </div>
 
       {/* 버튼 3 */}
       <div className={`absolute w-[70px] h-[70px] bg-gray-400 bottom-1/2 right-[20px] transition-all duration-500 ease-in-out cursor-pointer rounded-full flex items-center justify-center ${
         open ? 'opacity-100' : 'opacity-0'
-      }`}>
+      }`} onClick={()=>navigate('/Mypage')}>
         마이
       </div>
 
