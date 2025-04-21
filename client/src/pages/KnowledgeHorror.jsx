6f3805f27587d88dcfa7814d37f86361342807e6
윤종환 <yunjonghwan99@gmail.com>
2025-04-21 15:20:52 +0900

    메뉴 버튼으로 상식, 괴담에 접속할 경우 카테고리를 받아옴 받아온 데이터를 사용해서 화면을 표시해줌

--- client/src/pages/KnowledgeHorror.jsx
+++ client/src/pages/KnowledgeHorror.jsx
@@ -1,16 +1,17 @@
 import Menu from "../commonComponents/Menu.jsx";
+import { useLocation } from 'react-router-dom';
 
 function KnowledgeHorror() {
+  const location = useLocation();
+  const { category } = location.state || {};
+  
+
   return (
     <div>
       <Menu />
       <div>
-        <h1>Knowledge</h1>
-        <p>This is the Knowledge page.</p>
-      </div>
-      <div>
-        <h1>Horror</h1>
-        <p>This is the Horror page.</p>
+        <h1>{category}</h1>
+        <p>This is the {category} page.</p>
       </div>
     </div>
   );
