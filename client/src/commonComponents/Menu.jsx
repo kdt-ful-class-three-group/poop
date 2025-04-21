4ad7f18eabb3a069d789b694568f3da68c8f0d5e
윤종환 <yunjonghwan99@gmail.com>
2025-04-21 12:36:34 +0900

    카테고리를 생성하고 버튼을 눌렀을 경우 동작하는지 확인

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -1,5 +1,11 @@
 function Menu() {
-
+  return (
+    <div>
+      <button onClick={() => console.log('quiz')}>퀴즈</button>
+      <button onClick={() => console.log('common_sense')}>상식</button>
+      <button onClick={() => console.log('horror')}>괴담</button>
+    </div>
+  )
 }
 
 export default Menu;
