63171c4bea23c1502005f1c6dfc96a4f1c516bbd
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-22 16:28:18 +0900

    fetch로 데이터 가져와봄

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
+
 import QuizCard from "../components/QuizCard";
 import QuizButton from "../components/QuizButton";
 
 function Quiz() {
 
+  const [quizData, setQuizData] = useState([]); // 퀴즈를 저장할 상태 변수
+
+  // API 가져오기
+  useEffect(()=>{
+    const fetchQuiz = async() => {
+      try{
+        const response = await fetch('http://localhost:8080/quiz')
+        if(!response.ok){
+          throw new Error('네트워크 응답이 좋지 않습니다.');
+        }
+
+        const data = await response.json();
+
+        setQuizData(data);
+      }
+      catch(err){
+
+      }
+    }
+  },[])
+
+  //quiz에 지정하기
+
 
   return (
     <div className="max-w-[450px] m-auto w-full flex flex-col gap-10">
-      <QuizCard questTion={'문제보임'}/>
+      <QuizCard questTion={quizData[0]}/>
       <QuizButton />
     </div>
   );
