a5b2ba94e9a96f6f654a4974e2ab54fbf1ab196b
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-22 15:41:12 +0900

    QuizCard 가져와봄

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,8 +1,24 @@
+import QuizCard from "../components/QuizCard";
+
 function Quiz() {
+
+
   return (
     <div>
-      <h1>Quiz Page</h1>
-      <p>This is the quiz page.</p>
+      <QuizCard />
+      <div>
+        <div>
+          <input placeholder="정답 작성하기"></input>
+          <button>확인</button>
+        </div>
+        <p>다시 입력해주세요</p>
+      </div>
+      <div>
+        <button>정답보기</button>
+      </div>
+      <div>
+        <button>다음 문제로 넘어가기</button>
+      </div>
     </div>
   );
 }
