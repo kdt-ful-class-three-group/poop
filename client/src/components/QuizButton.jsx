52a4b95bd254032c5278471801ba81f688559c99
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-22 16:21:58 +0900

    기본 틀만 className으로 지정해둠

--- client/src/components/QuizButton.jsx
+++ client/src/components/QuizButton.jsx
@@ -1,8 +1,8 @@
 function QuizButton(){
   return(
-    <div>
+    <div className="w-11/12 m-auto flex flex-col gap-5 text-center">
       {/* 정답 작성 및 확인 */}
-      <div>
+      <div className="w-full border-1">
         <div>
           <input placeholder="정답 작성하기"></input>
           <button>확인</button>
@@ -10,11 +10,11 @@ function QuizButton(){
         <p>다시 입력해주세요</p>
       </div>
       {/* 정답보기 */}
-      <div>
+      <div className="w-full bg-gray-600">
         <button>정답보기</button>
       </div>
       {/* 문제 넘어가기 */}
-      <div>
+      <div className="w-full bg-gray-600">
         <button>다음 문제로 넘어가기</button>
       </div>
     </div>
