6be38391d68ca2029640141bf9a6cefaa7c69d5f
김재승 <jaes@gmail.com>
2025-04-22 15:47:40 +0900

    메뉴버튼 디자인 수정

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -7,10 +7,28 @@ function Menu() {
     navigate("/KnowledgeHorror", { state: { category } });
   };
   return (
-    <div>
-      <button onClick={() => navigate("/Quiz")}>퀴즈</button>
-      <button onClick={() => KnowledgeHorrorNav("knowledge")}>상식</button>
-      <button onClick={() => KnowledgeHorrorNav("horror")}>괴담</button>
+    <div
+      className="flex justify-between w-[calc(100%-80px)] m-auto"
+      style={{ margin: "30px auto 0" }}
+    >
+      <button
+        className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
+        onClick={() => navigate("/Quiz")}
+      >
+        퀴즈
+      </button>
+      <button
+        className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400 "
+        onClick={() => KnowledgeHorrorNav("knowledge")}
+      >
+        상식
+      </button>
+      <button
+        className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
+        onClick={() => KnowledgeHorrorNav("horror")}
+      >
+        괴담
+      </button>
     </div>
   );
 }
--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,8 +1,25 @@
 function Quiz() {
   return (
-    <div>
-      <h1>Quiz Page</h1>
-      <p>This is the quiz page.</p>
+    <div className="w-[calc(100%-80px)]">
+      <div className="shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px]">
+        <p>문제</p>
+      </div>
+      <div className="border-[1px] flex border-[#D9D9D9]/70">
+        <input
+          className="w-full"
+          type="text"
+          placeholder="정답을 입력해주세요."
+        />
+        <button className="bg-[#8E5E43] border-[#8E5E43] p-2 rounded-[0px_3px_3px_0px] text-white whitespace-nowrap">
+          확인
+        </button>
+      </div>
+      <div className="">
+        <button>정답확인</button>
+      </div>
+      <div className="">
+        <button>다음 문제</button>
+      </div>
     </div>
   );
 }
