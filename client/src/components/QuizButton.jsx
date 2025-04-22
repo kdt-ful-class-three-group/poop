6e8c85f9e7b5862ff1eb3f36f824b5ee50119cd3
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-22 15:46:09 +0900

    버튼 모아둔 컴포넌트 작성

--- client/src/components/QuizButton.jsx
+++ client/src/components/QuizButton.jsx
@@ -1,5 +1,24 @@
 function QuizButton(){
-
+  return(
+    <div>
+      {/* 정답 작성 및 확인 */}
+      <div>
+        <div>
+          <input placeholder="정답 작성하기"></input>
+          <button>확인</button>
+        </div>
+        <p>다시 입력해주세요</p>
+      </div>
+      {/* 정답보기 */}
+      <div>
+        <button>정답보기</button>
+      </div>
+      {/* 문제 넘어가기 */}
+      <div>
+        <button>다음 문제로 넘어가기</button>
+      </div>
+    </div>
+  )
 }
 
 export default QuizButton;
