e21ccdbc5c2104e33874ef2ded36fec114348f70
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-22 16:07:41 +0900

    className 수정

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -7,10 +7,10 @@ function Menu() {
     navigate("/KnowledgeHorror", { state: { category } });
   };
 
-  const buttonClass = 'cursor-pointer bg-gray-200 p-[5px_20px]'
+  const buttonClass = 'cursor-pointer bg-gray-200 pl-4 pr-4 pt-2 pb-2'
 
   return (
-    <div className="flex w-full m-auto max-w-[450px] justify-around mt-10 mb-10">
+    <div className="flex w-full m-auto max-w-[450px] justify-between mt-10 mb-10">
       <button className={buttonClass}
       onClick={() => navigate("/Quiz")}>퀴즈</button>
       <button className={buttonClass} onClick={() => KnowledgeHorrorNav("knowledge")}>상식</button>
