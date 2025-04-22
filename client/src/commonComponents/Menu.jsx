2714a2546e90827516e7a8c92d4f3f5a665fba7e
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-22 15:50:30 +0900

    화면이 커져도 가운데 위치함

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -10,7 +10,7 @@ function Menu() {
   const buttonClass = 'cursor-pointer bg-gray-200 p-[5px_20px]'
 
   return (
-    <div className="flex w-full justify-around mt-10 mb-10">
+    <div className="flex w-full m-auto max-w-[450px] justify-around mt-10 mb-10">
       <button className={buttonClass}
       onClick={() => navigate("/Quiz")}>퀴즈</button>
       <button className={buttonClass} onClick={() => KnowledgeHorrorNav("knowledge")}>상식</button>
