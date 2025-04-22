1b549beebc3545f9620c6773df56b5ba2e0f74c2
김재승 <jaes@gmail.com>
2025-04-22 17:15:56 +0900

    퀴즈,상식,괴담 전체 레이아웃 수정

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,12 +1,12 @@
 function Quiz() {
   return (
-    <div className="w-[calc(100%-80px)]">
-      <div className="shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px]">
-        <p>문제</p>
+    <div className="w-full">
+      <div className="shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px]  mb-[30px] p-4">
+        <p>전투기 조종사들이 제일 무서워하는 것은?</p>
       </div>
-      <div className="border-[1px] flex border-[#D9D9D9]/70">
+      <div className="border-[1px] flex border-[#D9D9D9]/70  mb-[60px] ">
         <input
-          className="w-full"
+          className="w-full outline-none p-2"
           type="text"
           placeholder="정답을 입력해주세요."
         />
@@ -14,11 +14,16 @@ function Quiz() {
           확인
         </button>
       </div>
-      <div className="">
+      <div className="flex justify-center bg-gray-300 px-4 py-2 rounded-md mb-[30px] hover:bg-gray-400">
         <button>정답확인</button>
       </div>
-      <div className="">
-        <button>다음 문제</button>
+      <div className="flex justify-between">
+        <button className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400">
+          이전 문제
+        </button>
+        <button className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400">
+          다음 문제
+        </button>
       </div>
     </div>
   );
