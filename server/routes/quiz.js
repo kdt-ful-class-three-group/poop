bb6a7046d2efeadda0fb31e6fddab0dbb340ec7d
최정민 <bigdragon00@naver.com>
2025-04-22 14:20:23 +0900

    데이터집어넣기

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -12,4 +12,25 @@ router.get("/", async (req, res) => {
   }
 });
 
+
+
++router.post("/", async (req, res) => {
+  const { question, answer } = req.body;
+
+  try {
+    const query = "INSERT INTO quiz(question, answer) VALUES (?, ?)";
+    const [result] = await pool.query(query, [ question, answer]);
+
+    res
+        .status(201)
+        .json({ success: true, quiz: {  question, answer } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, msg: "서버 오류" });
+  }
+});
+
 export default router;
+
+
+
