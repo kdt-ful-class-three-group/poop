200b488dc9b19d03135c6a2bb54eee368243a4dd
김윤지 <yunw0117@gmail.com>
2025-04-23 16:20:27 +0900

    dev pull하기 위한 커밋

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,49 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
-import Community from './pages/Community';
-import Home from './pages/Home';
-import Horror from './pages/Horror';
-import Knowledge from './pages/Knowledge';
-import Login from './pages/Login';
-import Quiz from './pages/Quiz';
-import Mypage from './pages/Mypage';
-import Admin from './pages/Admin';
-import Register from './pages/Register';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Community from "./pages/Community.jsx";
+import CommunityDetail from "./pages/CommunityDetail.jsx";
+import Home from "./pages/Home.jsx";
+import KnowledgeHorror from "./pages/KnowledgeHorror.jsx";
+import Login from "./pages/Login.jsx";
+import Quiz from "./pages/Quiz.jsx";
+import Mypage from "./pages/Mypage.jsx";
+import Admin from "./pages/Admin.jsx";
+import Register from "./pages/Register.jsx";
+import Layout from "./layout/Layout.jsx";
+import CommunityWrite from "./pages/CommunityWrite.jsx";
 
+import Nav from "./commonComponents/Nav.jsx";
 
 function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Home>
+          <Routes>
+            {/* Layout 없이 렌더링할 페이지들 */}
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/Community" element={<Community />} />
+            <Route
+              path="/Community/CommunityDetail"
+              element={<CommunityDetail />}
+            />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Mypage" element={<Mypage />} />
+            <Route path="/Admin" element={<Admin />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/CommunityWrite" element={<CommunityWrite />} />
 
-    return (
-        <div className = "App">
-            <Router>
-                <Routes>
-                    <Route path ="/" element={<Home />} />
-                    <Route path="/Community" element={<Community />} />
-                    <Router path="/Horror" element={<Horror />} />
-                    <Router path="/Knowledge" element = {<Knowledge />} />
-                    <Router path="/Login" element={<Login />} />
-                    <Router path="/Quiz" element={<Quiz />} />
-                    <Router path="/Mypage" element={<Mypage />} />
-                    <Router path="/Admin" element={<Admin />} />
-                    <Router path="/Register" element={<Register />} />
-                </Routes>
-
-            </Router>
-        </div>
-    )
+            {/* Layout 적용 구간: path 가 "/KnowledgeHorror" 또는 "/Quiz" 인 경우에만 */}
+            <Route element={<Layout />}>
+              <Route path="/KnowledgeHorror" element={<KnowledgeHorror />} />
+              <Route path="/Quiz" element={<Quiz />} />
+            </Route>
+          </Routes>
+        </Home>
+        <Nav />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/CommunityDetail.jsx
@@ -0,0 +1,10 @@
+function CommunityDetail() {
+  return (
+    <div>
+      <h1>CommunityDetail Page</h1>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+export default CommunityDetail;
