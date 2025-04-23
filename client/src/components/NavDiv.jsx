5383de2757e84c2482b75d83c44a44e4b015eda3
김민지 <twoji3019@naver.com>
2025-04-23 15:28:33 +0900

    nav 스타일 수정해좀

--- client/src/components/NavDiv.jsx
+++ client/src/components/NavDiv.jsx
@@ -9,12 +9,12 @@ function NavDiv({navRef, btnClick, closeNav}){
     return(
       <div
         ref={navRef} // 이 안에 있는 요소는 이벤트 방지함
-        className={`bg-[#D9D9D9] absolute flex-col items-center bottom-0 justify-center p-4 w-full rounded-t-full h-[230px] max-w-[450px] max-h-[450px] transition-all duration-300 ease-in-out ${
+        className={`bg-[#D9D9D9] absolute flex-col items-center bottom-0 justify-center gap-20 p-4 w-full rounded-t-full h-[230px] max-w-[450px] max-h-[450px] transition-all duration-300 ease-in-out ${
           btnClick ? "flex opacity-100 pointer-events-auto" : "flex pointer-events-none opacity-0"
         }`}
       >
         <div className="flex">
-          <button className="cursor-pointer"
+          <button className="cursor-pointer hover:text-[#8E5E43]"
             onClick={() => {
               navigate("/Community");
             }}
@@ -22,18 +22,24 @@ function NavDiv({navRef, btnClick, closeNav}){
             커뮤니티
           </button>
         </div>
-        <div className="flex justify-between items-center">
-          <button className="cursor-pointer"
+        <div className="flex justify-between items-center w-full">
+          <button className="cursor-pointer  hover:text-[#8E5E43]"
             onClick={() => {
-              navigate("/Quiz");
-            }}
-          >
-            유머
-          </button>
-          <button className="cursor-pointer" onClick={closeNav}>닫기</button>
-          <button
-            className="cursor-pointer"
-            onClick={() => {
+                navigate("/Quiz");
+              }}
+              className="flex-1 text-center"
+              >
+              유머
+              </button>
+              <button
+              className="flex-1 text-center cursor-pointer  hover:text-[#8E5E43]"
+              onClick={closeNav}
+              >
+              닫기
+              </button>
+              <button
+              className="flex-1 text-center cursor-pointer  hover:text-[#8E5E43]"
+              onClick={() => {
               navigate("/Mypage");
             }}
           >
