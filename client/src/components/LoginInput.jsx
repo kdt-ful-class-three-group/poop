283c643ae56743547b2ed9fa3057d1168b525da1
김민지 <twoji3019@naver.com>
2025-04-23 15:40:47 +0900

    로그인 input에 required 추가

--- client/src/components/LoginInput.jsx
+++ client/src/components/LoginInput.jsx
@@ -8,9 +8,9 @@ function LoginInput(){
   return(
     <form className="flex flex-col gap-2">
       <input type="text" placeholder="아이디"
-        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100"></input>
+        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" required></input>
       <input type='password' placeholder="비밀번호"
-        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100"></input>
+        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" required></input>
       <Button text='로그인' colorClass = "bg-gray-300"/>
     </form>
   )
