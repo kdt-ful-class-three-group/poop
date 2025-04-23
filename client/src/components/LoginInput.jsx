39fe9c7dfcfdd7b80d489b43e1e590cfa23740b8
김민지 <twoji3019@naver.com>
2025-04-23 11:07:54 +0900

    input 작성

--- client/src/components/LoginInput.jsx
+++ client/src/components/LoginInput.jsx
@@ -1,5 +1,14 @@
 function LoginInput(){
+  // 아이디 입력
+  // 비밀번호 입력
+  // 로그인 버튼
 
+  return(
+    <div>
+      <input type="text" placeholder="아이디"></input>
+      <input type='password' placeholder="비밀번호"></input>
+    </div>
+  )
 }
 
 export default LoginInput;
