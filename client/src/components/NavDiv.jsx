551d18912760ea707a5bb7c2bd2a029bf0139fb3
김민지 <twoji3019@naver.com>
2025-04-23 15:31:00 +0900

    hover했을 때 스타일 변경

--- client/src/components/NavDiv.jsx
+++ client/src/components/NavDiv.jsx
@@ -9,12 +9,12 @@ function NavDiv({navRef, btnClick, closeNav}){
     return(
       <div
         ref={navRef} // 이 안에 있는 요소는 이벤트 방지함
-        className={`bg-[#D9D9D9] absolute flex-col items-center bottom-0 justify-center gap-20 p-4 w-full rounded-t-full h-[230px] max-w-[450px] max-h-[450px] transition-all duration-300 ease-in-out ${
+        className={`bg-gray-200 absolute flex-col items-center bottom-0 justify-center gap-20 p-4 w-full rounded-t-full h-[230px] max-w-[450px] max-h-[450px] transition-all duration-300 ease-in-out ${
           btnClick ? "flex opacity-100 pointer-events-auto" : "flex pointer-events-none opacity-0"
         }`}
       >
         <div className="flex">
-          <button className="cursor-pointer hover:text-[#8E5E43]"
+          <button className="cursor-pointer hover:font-bold"
             onClick={() => {
               navigate("/Community");
             }}
@@ -23,22 +23,21 @@ function NavDiv({navRef, btnClick, closeNav}){
           </button>
         </div>
         <div className="flex justify-between items-center w-full">
-          <button className="cursor-pointer  hover:text-[#8E5E43]"
+          <button className="cursor-pointer flex-1 text-center hover:font-bold"
             onClick={() => {
                 navigate("/Quiz");
               }}
-              className="flex-1 text-center"
               >
               유머
               </button>
               <button
-              className="flex-1 text-center cursor-pointer  hover:text-[#8E5E43]"
+              className="flex-1 text-center cursor-pointer  hover:font-bold"
               onClick={closeNav}
               >
               닫기
               </button>
               <button
-              className="flex-1 text-center cursor-pointer  hover:text-[#8E5E43]"
+              className="flex-1 text-center cursor-pointer  hover:font-bold"
               onClick={() => {
               navigate("/Mypage");
             }}
