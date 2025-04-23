2e28c941e7f1609ff69e86423be87211375863e9
김민지 <twoji3019@naver.com>
2025-04-23 16:50:25 +0900

    스타일 수정 중

--- client/src/components/Email.jsx
+++ client/src/components/Email.jsx
@@ -2,13 +2,13 @@ import Button from "./Button";
 
 function Email(){
   return(
-    <div>
+    <div className="flex flex-col gap-4 w-full">
       {/* 이메일 */}
       <form>
-        <p>이메일</p>
-        <div>
-          <input type="text" />
-          <Button text='인증받기' colorClass={'bg-gray-300'}/>
+        <p className="text-sm">이메일</p>
+        <div className="flex">
+          <input type="text" className="w-10/12" />
+          <Button text='인증' colorClass={'bg-gray-300'}/>
         </div>
         <p>이메일 형식과 일치하지 않습니다</p>
       </form>
