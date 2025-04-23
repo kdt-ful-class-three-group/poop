154d969c2fa3a2a28ddf6fd401ba497ce1597849
윤종환 <yunjonghwan99@gmail.com>
2025-04-23 15:42:05 +0900

    merge 작업 진행 : issue13 -> dev

--- client/src/pages/KnowledgeHorror.jsx
+++ client/src/pages/KnowledgeHorror.jsx
@@ -6,8 +6,18 @@ function KnowledgeHorror() {
   const category = location.state?.category || "없음"; // 기본값 설정
 
   return (
-    <div>
-      <h1>선택된 카테고리: {category}</h1>
+    <div className="w-full h-[calc(100vh-230px)] flex items-center">
+      <div className="absolute left-1">
+        <button className="text-6xl">&#8249;</button>
+      </div>
+      <div className="w-full h-[100%] shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px] flex items-center justify-center">
+        <p className="">
+          내용
+        </p>
+      </div>
+      <div className="absolute right-1">
+        <button className="text-6xl">&#8250;</button>
+      </div>
     </div>
   );
 }
