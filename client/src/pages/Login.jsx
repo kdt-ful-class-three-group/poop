448895bedef58e8eaedbbd4226d452f2f257ad94
김민지 <twoji3019@naver.com>
2025-04-23 14:30:02 +0900

    login에서 컴포넌트 사용

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,8 @@ import LoginInput from "../components/LoginInput";
 import FindAccount from "../components/FindAccount";
 // 간편 로그인
 import EasyLogin from "../components/EasyLogin";
+//버튼 컴포넌트
+import Button from "../components/Button";
 
 import { useNavigate } from "react-router-dom";
 
@@ -24,10 +26,7 @@ function Login() {
       <LoginInput/>
       <FindAccount/>
       <EasyLogin/>
-      <button onClick={()=> navigate("/Register")}
-        className="px-4 py-2 rounded-md cursor-pointer hover:bg-gray-400">
-        회원가입
-      </button>
+      <Button text='회원가입' colorClass={''} clickEvent={()=>navigate('/Register')}/>
     </div>
   );
 }
