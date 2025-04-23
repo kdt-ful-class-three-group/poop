edf73bdf2e6046c544202151cdf2078ddd3fa726
김재승 <jaes@gmail.com>
2025-04-23 11:09:43 +0900

    home.jsx 메인 레이아웃 적용

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Community from './pages/Community.jsx';
-import Write from './pages/Write.jsx';
 import Home from './pages/Home.jsx';
 import KnowledgeHorror from './pages/KnowledgeHorror.jsx';
 import Login from './pages/Login.jsx';
@@ -15,12 +14,17 @@ import Nav from "./commonComponents/Nav.jsx";
 
 function App() {
   return (
-      <div className="App">
-        <Router>
+    <div className="App">
+      <Router>
+        <Home>
           <Routes>
             {/* Layout 없이 렌더링할 페이지들 */}
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/Community" element={<Community />} />
+            <Route
+              path="/Community/CommunityDetail"
+              element={<CommunityDetail />}
+            />
             <Route path="/Login" element={<Login />} />
             <Route path="/Mypage" element={<Mypage />} />
             <Route path="/Admin" element={<Admin />} />
@@ -30,12 +34,13 @@ function App() {
             {/* Layout 적용 구간: path 가 "/KnowledgeHorror" 또는 "/Quiz" 인 경우에만 */}
             <Route element={<Layout />}>
               <Route path="/KnowledgeHorror" element={<KnowledgeHorror />} />
-              <Route path="/Quiz"element={<Quiz />} />
+              <Route path="/Quiz" element={<Quiz />} />
             </Route>
           </Routes>
-          <Nav/>
-        </Router>
-      </div>
+        </Home>
+        <Nav />
+      </Router>
+    </div>
   );
 }
 
