27da66cdf8dbeb92fc8cd9eb887ad0f6b88abd2e
김재승 <jaes@gmail.com>
2025-04-23 11:09:43 +0900

    home.jsx 메인 레이아웃

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -5,13 +5,13 @@ import Nav from "../commonComponents/Nav.jsx";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col items-center h-[calc(100vh-120px)] w-full px-[30px] m-auto">
+    <>
       <Menu />
       <div className="flex justify-center items-center w-full mt-[40px]">
         {/* 중첩된 Route 컴포넌트들이 이 자리에 렌더링 함 */}
         {/* Outlet 컴포넌트는 중첩된 Route의 자식 컴포넌트를 렌더링하는 역할을 함 */}
         <Outlet />
       </div>
-    </div>
+    </>
   );
 }
