927d2cc4533ef975ff3ebe1f82e46a6d0281f72d
김민지 <twoji3019@naver.com>
2025-04-23 11:31:14 +0900

    간편로그인 버튼 추가

--- client/src/components/EasyLogin.jsx
+++ client/src/components/EasyLogin.jsx
@@ -1,5 +1,11 @@
 function EasyLogin(){
-  
+  return(
+    <div>
+      <button>카카오톡으로 계속하기</button>
+      <button>네이버로 계속하기</button>
+      <button>구글로 계속하기</button>
+    </div>
+  )
 }
 
 export default EasyLogin;
