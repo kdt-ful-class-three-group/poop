8d3bf5b13450e9d1dbf2ae3a709f0828a720f586
김민지 <twoji3019@naver.com>
2025-04-23 16:30:45 +0900

    이메일 작성 폼

--- client/src/components/Email.jsx
+++ client/src/components/Email.jsx
@@ -2,16 +2,23 @@ import Button from "./Button";
 
 function Email(){
   return(
-    <form>
-      <div>
+    <div>
+      {/* 이메일 */}
+      <form>
         <p>이메일</p>
         <div>
           <input type="text" />
           <Button text='인증받기' colorClass={'bg-gray-300'}/>
         </div>
         <p>이메일 형식과 일치하지 않습니다</p>
+      </form>
+      {/* 이메일 인증 */}
+      <div>
+        <p>이메일 인증</p>
+        <input type='text'/>
+        <p>다시 입력해주세요</p>
       </div>
-    </form>
+    </div>
   )
 }
 
