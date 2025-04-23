7de7d0b2549623ad52c507b9349f695d9b598dae
김민지 <twoji3019@naver.com>
2025-04-23 14:28:01 +0900

    EsayLogin에 버튼 컴포넌트 사용 & 배열메서드로 작성

--- client/src/components/EasyLogin.jsx
+++ client/src/components/EasyLogin.jsx
@@ -1,18 +1,21 @@
+import Button from "./Button";
+
 function EasyLogin(){
+
+  const ButtonTexts= [
+    "카카오톡으로 계속하기",
+    "네이버로 계속하기",
+    "구글로 계속하기"
+  ]
+
   return(
     <div className="flex flex-col gap-2">
-      <button 
-        className="bg-gray-300 px-4 rounded-md py-2 cursor-pointer hover:bg-gray-400">
-          카카오톡으로 계속하기
-      </button>
-      <button
-        className="bg-gray-300 px-4 rounded-md py-2 cursor-pointer hover:bg-gray-400">
-          네이버로 계속하기
-      </button>
-      <button
-        className="bg-gray-300 px-4 rounded-md py-2 cursor-pointer hover:bg-gray-400">
-          구글로 계속하기
-      </button>
+      {ButtonTexts.map((text,i)=>(
+        <Button
+          key={i}
+          text={text}
+          colorClass='bg-gray-300'></Button>
+      ))}
     </div>
   )
 }
