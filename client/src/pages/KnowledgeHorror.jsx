95190a11aa89eccfa483dcb6c8a89ba2baa23a2b
윤종환 <yunjonghwan99@gmail.com>
2025-04-23 14:56:13 +0900

    상식 및 괴담 페이지 만들기

--- client/src/pages/KnowledgeHorror.jsx
+++ client/src/pages/KnowledgeHorror.jsx
@@ -6,8 +6,20 @@ function KnowledgeHorror() {
   const category = location.state?.category || "없음"; // 기본값 설정
 
   return (
-    <div>
-      <h1>선택된 카테고리: {category}</h1>
+    <div className="w-full h-[calc(100vh-230px)] flex items-center">
+      <div className="flex items-center justify-center">
+        <button className="text-7xl">&#8249;</button>
+      </div>
+      <div className="w-full h-[100%] border-1 flex items-center justify-center">
+        <p className="">
+          내용
+        </p>
+      </div>
+      <div>
+        <div className="flex items-center justify-center">
+          <button className="text-7xl">&#8250;</button>
+        </div>
+      </div>
     </div>
   );
 }
