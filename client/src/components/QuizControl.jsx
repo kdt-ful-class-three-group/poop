a8a92f75fdf83cb56c8d66b0a5518d223d320db1
김민지 <twoji3019@naver.com>
2025-04-24 16:43:03 +0900

    props 전달할 때 {} 사용

--- client/src/components/QuizControl.jsx
+++ client/src/components/QuizControl.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-function QuizControl(btnText, answerBtn, prevBtn, nextBtn){
+function QuizControl({btnText, answerBtn, prevBtn, nextBtn}){
   return(
     <div className="flex flex-col gap-5">
       <Button text={btnText} colorClass={"bg-gray-300 w-full"} clickEvent={answerBtn}/>
