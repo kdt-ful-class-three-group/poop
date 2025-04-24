646071d12c5204d5de6848a3b6433e0d51c8de6b
윤종환 <yunjonghwan99@gmail.com>
2025-04-24 14:39:58 +0900

    생년월일 폼을 기준으로 가져왔기에 닉네임을 입력하는 폼으로 수정 함

--- client/src/pages/RegisterNickname.jsx
+++ client/src/pages/RegisterNickname.jsx
@@ -7,11 +7,11 @@ const RegisterNickname = () => {
       <button className="mb-4 px-4 py-2 bg-gray-300 rounded">이전</button>
       {/* 2. 생년월일 */}
       <div className="mb-4">
-        <label className="block font-bold mb-2">생년월일</label>
+        <label className="block font-bold mb-2">닉네임</label>
         <input
           type="text"
           name="birthdate"
-          placeholder="예: 19990101"
+          placeholder="닉네임을 입력해 주세요"
           maxLength="8"
           className="w-full p-2 border rounded bg-gray-200"
         />
