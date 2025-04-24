a71544fc5f89970e16b86a801680c96234dd2a99
김윤지 <yunw0117@gmail.com>
2025-04-24 10:28:21 +0900

    회원가입 성별, 생년월일 만듦

--- client/src/pages/RegisterBirth.jsx
+++ client/src/pages/RegisterBirth.jsx
@@ -1,21 +1,62 @@
 // RegisterBirth.jsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterBirth = () => {
-  return (
-    <div className="m-3 w-full ">
-      <button className="mb-4 px-4 py-2 bg-gray-300 rounded">이전</button>
+  const navigate = useNavigate();
+  const [gender, setGender] = React.useState('');
+  const [birthdate, setBirthdate] = React.useState('');
 
+  // 다음 버튼
+  const handeleNext = () => {
+    // 이동만 처리
+    navigate('/RegisterNickname');
+  };
+
+  // 이전 버튼
+  const hadlePrev = () => {
+    navigate('/RegisterEmail')
+  };
+
+
+  return (
+
+    <div className="m-3 w-full ">
+      <button  
+      onClick={hadlePrev}
+      className="mb-4 px-4 py-2 bg-gray-300 rounded"
+      >
+      이전
+      </button>
+    
       {/* 1. 성별 */}
       <div className="mb-4">
         <label className="block font-bold mb-2 mt-3">성별</label>
         <div className="flex gap-4">
           <label className="flex items-center">
-            <input type="radio" name="gender" value="male" className="mr-2" />
+
+
+            <input 
+            type="radio" 
+            name="gender" 
+            value="male" 
+            checked = {gender === 'male'}
+            onClick={(e) => setGender(e.target.value)}
+            className="mr-2" 
+            />
             남자
           </label>
+
+
           <label className="flex items-center">
-            <input type="radio" name="gender" value="female" className="mr-2" />
+            <input 
+            type="radio" 
+            name="gender" 
+            value="female" 
+            className="mr-2"
+            checked = {gender === 'female'}
+            onClick={(e) => setGender(e.target.value)}
+            />
             여자
           </label>
         </div>
@@ -30,12 +71,23 @@ const RegisterBirth = () => {
           placeholder="예: 19990101"
           maxLength="8"
           className="w-full p-2 border rounded bg-gray-200"
+          value={birthdate}
+          onChange={(e) => setBirthdate(e.target.value)}
         />
       </div>
 
       {/* 3. 다음 버튼 */}
       <div>
-        <button className="w-full py-2 bg-gray-300 rounded">다음</button>
+        <button
+        onClick={handeleNext}
+        disabled = {!gender || !birthdate}
+        className={`w-full py-2 rounded ${gender ? 'bg-yellow-900 text-white' : 'bg-gray-500 cursor-not-allowed'}`}
+        >
+      
+        다음
+        </button>
+
+
       </div>
     </div>
   );
