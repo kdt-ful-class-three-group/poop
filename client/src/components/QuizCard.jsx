c40cee18bd6135a1c66bf29a6dbb29aed3a32977
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-24 13:02:13 +0900

    기본값 줌

--- client/src/components/QuizCard.jsx
+++ client/src/components/QuizCard.jsx
@@ -1,7 +1,7 @@
 function QuizCard({quizData}) {
   return(
-    <div className="shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px]  mb-[30px] p-4" key={quizData.quiz_id}>
-      <p>{quizData.question}</p>
+    <div className="shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px]  mb-[30px] p-4" key={quizData?.quiz_id || 0}>
+      <p>{quizData?.question|| ''}</p>
     </div>
   )
 }
