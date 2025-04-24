4aeb616a45f61592b0995a984f339a8162d7a247
윤종환 <yunjonghwan99@gmail.com>
2025-04-24 12:03:23 +0900

    form 태그의 max-w-md 클래스 삭제

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,10 +5,10 @@ function Register() {
 
     return (
         <div className=" w-full h-full">
-            <h1 className="justify-start items-left mt-20    ">회원가입</h1>
+            <h1 className="justify-start items-left mt-20">회원가입</h1>
             <div className="h-100 flex flex-col justify-center items-center">
 
-                <form className="w-full max-w-md">
+                <form className="w-full">
                     <label className="text-sm text-black mb-2 ">아이디</label>
                     <Input type="text" name="username"/>
                     <label className="text-sm text-black mb-2">비밀번호</label>
