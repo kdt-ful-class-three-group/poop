c0ee091788d0014647b4339ef6f39caa0e28d4eb
김민지 <root@LAPTOP-DBJHGFBP>
2025-04-24 12:47:04 +0900

    스타일 Quiz처럼 변경

--- client/src/components/QuizButton.jsx
+++ client/src/components/QuizButton.jsx
@@ -2,21 +2,25 @@ import Button from "./Button";
 
 function QuizButton({nextBtn, prevBtn}){
   return(
-    <div className="w-11/12 m-auto flex flex-col gap-5 text-center">
+    <div className="flex flex-col gap-5 text-center">
       {/* 정답 작성 및 확인 */}
-      <div className="w-full border-1">
-        <div>
-          <input placeholder="정답 작성하기"></input>
-          <button>확인</button>
+      <div className="w-full">
+        <div className="flex border-[1px] border-[#D9D9D9]/70">
+          <input
+            className="w-full outline-none p-2"
+            type="text"
+            placeholder="정답을 입력해주세요."
+          />
+          <button className="bg-[#8E5E43] border-[#8E5E43] p-2 rounded-[0px_3px_3px_0px] text-white whitespace-nowrap">확인</button>
         </div>
-        <p>다시 입력해주세요</p>
+        <p className="text-left text-red-500 text-sm">다시 입력해주세요</p>
       </div>
       {/* 정답보기 */}
-      <Button text="정답확인" colorClass={"bg-gray-300"}/>
+      <Button text="정답확인" colorClass={"bg-gray-300 w-full"}/>
       {/* 문제 넘어가기 */}
-      <div className="w-full">
-        <Button text ='이전 문제' colorClass={"bg-gray-300"} clickEvent={()=>prevBtn()}/>
-        <Button text ='다음 문제' colorClass={"bg-gray-300"} clickEvent={()=>nextBtn()}/>
+      <div className="flex justify-between">
+        <Button text ='이전 문제' colorClass={"bg-gray-300 w-[45%]"} clickEvent={prevBtn}/>
+        <Button text ='다음 문제' colorClass={"bg-gray-300 w-[45%]"} clickEvent={nextBtn}/>
       </div>
     </div>
   )
