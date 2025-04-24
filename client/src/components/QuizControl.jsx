6de90c38ed94bf64f3b3c2a740c8692bf46eadc2
김민지 <twoji3019@naver.com>
2025-04-24 15:52:40 +0900

    버튼 분리

--- client/src/components/QuizControl.jsx
+++ client/src/components/QuizControl.jsx
@@ -1,5 +1,15 @@
-function QuizControl(){
+import Button from "./Button";
 
+function QuizControl(){
+  return(
+    <div>
+      <Button text='정답확인' colorClass={"bg-gray-300 w-full"}/>
+      <div className="flex justify-between">
+        <Button text ='이전 문제' colorClass={"bg-gray-300 w-[45%]"} clickEvent={() => {}}/>
+        <Button text ='다음 문제' colorClass={"bg-gray-300 w-[45%]"} clickEvent={() => {}}/>
+      </div>
+    </div>
+  )
 }
 
 export default QuizControl;
