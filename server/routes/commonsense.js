9d45e3fab42a5f2f85faa585d8d3c649177f88b9
김민지 <twoji3019@naver.com>
2025-04-28 10:58:21 +0900

    기본 틀 작성

--- server/routes/commonsense.js
+++ server/routes/commonsense.js
@@ -1,3 +1,23 @@
 import express from 'express'
 import pool from '../config/database.js'
 const router = express.Router()
+
+// get으로 조회
+router.get('/',async(req,res)=>{
+  try{
+
+  }
+  catch(err){
+
+  }
+})
+
+//post로 데이터 추가
+router.post('/',async(req,res)=>{
+  try{
+
+  }
+  catch(err){
+    
+  }
+})
