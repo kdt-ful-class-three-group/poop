1246cae4b24b4eef726d93623b63b95ea8f85345
김민지 <twoji3019@naver.com>
2025-04-28 14:18:00 +0900

    뼈대 작성

---
+++ server/routes/community.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import pool from '../config/database.js'
+const router = express.Router()
+
+router.get('/',async(req,res)=>{
+  try{
+
+  }
+  catch(err){
+
+  }
+})
+
+router.post('/',async(req,res)=>{
+  try{
+
+  }
+  catch(err){
+    
+  }
+})
+
+export default router
