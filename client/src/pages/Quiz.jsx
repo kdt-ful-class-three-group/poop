3b7da06b6bde46d37be9a3649135d40f231df531
윤종환 <yunjonghwan99@gmail.com>
2025-04-28 09:18:08 +0900

    문제 이동

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -5,19 +5,30 @@ import QuizButton from "../components/QuizButton";
 
 function Quiz() {
   const [quizData, setQuizData] = useState(null);
+  const [moveData, setMoveData] = useState(0);
 
   useEffect(() => {
   fetch("http://localhost:8081/quiz").then((response) => response.json())
       .then((data) => {
         setQuizData(data);
       })
-  }, []);
+  }, [moveData]);
   //quiz에 지정하기
+
+  const nextEvent = () => {
+    setMoveData(moveData + 1);
+    console.log('next');
+  }
+
+  const prevEvent = () => {
+    setMoveData(moveData - 1)
+    console.log('prev');
+  }
 
   return (
     <div className="w-full">
       <div className="shadow-[0px_0px_4px_rgba(0,0,0,0.25)] rounded-[6px]  mb-[30px] p-4">
-      {quizData ? <p>{quizData[0].question}</p> : <p>데이터 없음</p>}
+      {quizData ? <p>{quizData[moveData].question}</p> : <p>데이터 없음</p>}
       </div>
       <div className="border-[1px] flex border-[#D9D9D9]/70  mb-[60px] ">
         <input
@@ -33,10 +44,10 @@ function Quiz() {
         <button>정답확인</button>
       </div>
       <div className="flex justify-between">
-        <button className="bg-gray-300 w-[45%] px-4 py-2 rounded-md hover:bg-gray-400">
+        <button className="bg-gray-300 w-[45%] px-4 py-2 rounded-md hover:bg-gray-400" onClick={prevEvent}>
           이전 문제
         </button>
-        <button className="bg-gray-300 w-[45%] px-4 py-2 rounded-md hover:bg-gray-400">
+        <button className="bg-gray-300 w-[45%] px-4 py-2 rounded-md hover:bg-gray-400" onClick={nextEvent}>
           다음 문제
         </button>
       </div>
