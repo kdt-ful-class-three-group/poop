9cc6b15446354395f1197eece6fd8100b4ae3a83
윤종환 <yunjonghwan99@gmail.com>
2025-04-28 12:54:35 +0900

    quiz 라우터 작성

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -12,4 +12,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const { question, answer } = req.body
+
+  //값이 없을 때
+  if (!question || !answer) {
+    return res.status(400).json('데이터가 존재하지 않습니다.')
+  }
+
+  try {
+    await pool.execute('INSERT INTO quiz (question, answer) VALUES (?,?)', [question, answer])
+    // 성공시 응답
+    res.status(201).json('전송 성공');
+  }
+  catch (err) {
+    console.error(err)
+    res.status(500).json('전송 실패');
+  }
+})
+
 export default router;
