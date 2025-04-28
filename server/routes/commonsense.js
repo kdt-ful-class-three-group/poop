d71c5f5cb29a44aebcf2e432169fd2ead2c4a034
김민지 <twoji3019@naver.com>
2025-04-28 11:12:29 +0900

    insert 구문 추가

--- server/routes/commonsense.js
+++ server/routes/commonsense.js
@@ -10,17 +10,21 @@ router.get('/',async(req,res)=>{
   }
   catch(err){
     console.error(err)
-    res.status(500).json({error:'server error'})
+    res.status(500).json({error:'데이터 조회 실패'})
   }
 })
 
 //post로 데이터 추가
 router.post('/',async(req,res)=>{
+  const {question, answer} = req.body
   try{
-
+    const [rows] = await pool.query('INSERT INTO COMMON_SENSE VALUES (?,?)',[question,answer])
+    // 성공시 응답
+    res.status(201).json({message:'데이터 추가 완료'})
   }
   catch(err){
-
+    console.error(err)
+    res.status(500).json({error:'데이터 추가 실패'})
   }
 })
 
