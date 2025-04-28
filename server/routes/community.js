639eea4d6bdbd63b2572d866dfeca46ab647b46f
김민지 <twoji3019@naver.com>
2025-04-28 14:22:19 +0900

    데이터 조회 & 추가 작성

--- server/routes/community.js
+++ server/routes/community.js
@@ -4,19 +4,25 @@ const router = express.Router()
 
 router.get('/',async(req,res)=>{
   try{
-
+    const [rows] = await pool.execute('SELECT * FROM BOARD')
+    res.json(rows)
   }
   catch(err){
-
+    console.error(err)
+    res.status(500).json({err:'데이터 조회 실패'})
   }
 })
 
 router.post('/',async(req,res)=>{
-  try{
 
+  const {title,content} = req.body
+  try{
+    const [rows] = await pool.execute('INSERT INTO BOARD (usder_id,title,content) VALUES (?,?,?)',[2,title,content])
+    res.status(201).json({message:'데이터 추가 완료'})
   }
   catch(err){
-    
+    console.error(err)
+    res.status(500).json({error:'데이터 추가 실패'})
   }
 })
 
