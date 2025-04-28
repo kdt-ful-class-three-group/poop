7b0112db1b9d916306e8abdc5e99b9731c215a95
김민지 <twoji3019@naver.com>
2025-04-28 16:42:23 +0900

    스타일 추가

--- client/src/components/ToastPopup.jsx
+++ client/src/components/ToastPopup.jsx
@@ -1,6 +1,8 @@
 function ToastPopup({text, isShow, textColor}){
   return(
-    <div className={`${isShow ? 'block' : 'hidden'}` }>
+    <div
+      className={`${isShow ? 'block' : 'hidden'} absolute top-40 left-1/2 bg-black/70 transform -translate-x-1/2 rounded-md px-4 py-2`}
+    >
       <p className={textColor}>{text}</p>
     </div>
   )
