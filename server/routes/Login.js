b1dbcd770b2f05f616522efad7980f74a15ac10f
김민지 <twoji3019@naver.com>
2025-04-30 11:44:21 +0900

    특정 데이터 조회 추가

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -4,12 +4,34 @@ import express from 'express'
 import pool from '../config/database.js'
 const router = express.Router()
 
-router.get('/',(req,res)=>{
+//모든 데이터조회
+router.get('/',async (req,res)=>{
+  try{
 
+  }
+  catch(err){
+
+  }
 })
 
-router.post('/',(req,res)=>{
+//특정데이터 조회 - user_id로 확인
+router.get('/:user_id',async(req,res)=>{
+  try{
 
+  }
+  catch(err){
+
+  }
+})
+
+//데이터 추가
+router.post('/',async(req,res)=>{
+  try{
+
+  }
+  catch(err){
+
+  }
 })
 
 export default router
