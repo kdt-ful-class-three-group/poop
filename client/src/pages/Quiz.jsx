22cf942ebbbff5a053693a55d6dd87993d23c49a
김민지 <twoji3019@naver.com>
2025-04-30 15:42:36 +0900

    useLocation 사용 > 버튼 누르면 데이터 변경 완료

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
 
 import QuizCard from "../components/QuizCard";
 import QuizButton from "../components/QuizButton";
 
+
 function Quiz() {
+  const location = useLocation()
 
   const [data ,setData] = useState([]);
   const [num, setNum] = useState(Number(localStorage.getItem('Quiznum'))||0);
+
+  const [category,setCategory] = useState('quiz')
+
+  //버튼을 누를때마다 category가져오기
+  useEffect(()=>{
+    const selected = location.state?.category || 'quiz'
+    setCategory(selected)
+  },[location.state?.category])
 
   //로컬스토리지 - num이 변할 때마다 데이터 저장
   useEffect(()=>{
@@ -15,13 +26,13 @@ function Quiz() {
 
   //fetch 가져오기
   useEffect(()=>{
-    fetch('http://localhost:8080/quiz')
+    fetch(`http://localhost:8080/${category}`)
     .then((res)=>res.json())
     .then((data)=>{
       setData(data);
     })
 
-  },[])
+  },[category])
 
   //버튼 이벤트
   const prevBtn = ()=>{
