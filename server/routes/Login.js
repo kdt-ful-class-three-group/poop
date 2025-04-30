5e71b2e970744b4eba96cf1477f31130eba0f24a
김민지 <twoji3019@naver.com>
2025-04-30 11:44:38 +0900

    쿼리 바인딩 사용으로 수정

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -7,17 +7,19 @@ const router = express.Router()
 //모든 데이터조회
 router.get('/',async (req,res)=>{
   try{
-
+    const [rows] = await pool.execute('SELECT * FROM USER')
+    res.json(rows)
   }
   catch(err){
-
+    console.error(err)
   }
 })
 
 //특정데이터 조회 - user_id로 확인
 router.get('/:user_id',async(req,res)=>{
+  const userId = req.params.user_id
   try{
-
+    const [rows] = await pool.execute('SELECT user_id, password FROM USER WHERE user_id = ? ',[userId])
   }
   catch(err){
 
