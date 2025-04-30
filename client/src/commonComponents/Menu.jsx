48d7587c42c55392376b0f32a0ecd58131509bbc
김민지 <twoji3019@naver.com>
2025-04-30 15:39:22 +0900

    경로 동일하도록 다시 수정

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -13,19 +13,19 @@ function Menu() {
     <div className="flex justify-between w-full ">
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        onClick={() => Humor('/Quiz?category=quiz','quiz')}
+        onClick={() => Humor('/Quiz','quiz')}
       >
         퀴즈
       </button>
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400 "
-        onClick={() => Humor('/Quiz?category=sense','sense')}
+        onClick={() => Humor('/Quiz','sense')}
       >
         상식
       </button>
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        onClick={() => Humor('/Quiz?category=horror','horror')}
+        onClick={() => Humor('/Quiz','horror')}
       >
         괴담
       </button>
