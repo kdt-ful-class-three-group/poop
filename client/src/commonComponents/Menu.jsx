53c9a77343f8e8aa8308f8bd2ba6ed4636117145
김민지 <twoji3019@naver.com>
2025-04-30 15:37:26 +0900

    버튼 클릭하면 경로 변경 + 값 전달

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -5,27 +5,27 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 function Menu() {
   const navigate = useNavigate();
-  const KnowledgeHorrorNav = (category) => {
+  const Humor = (url,category) => {
     // navText(category);
-    navigate("/KnowledgeHorror", { state: { category } });
+    navigate(url, { state: { category } });
   };
   return (
     <div className="flex justify-between w-full ">
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        onClick={() => navigate("/Quiz")}
+        onClick={() => Humor('/Quiz?category=quiz','quiz')}
       >
         퀴즈
       </button>
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400 "
-        onClick={() => KnowledgeHorrorNav("knowledge")}
+        onClick={() => Humor('/Quiz?category=sense','sense')}
       >
         상식
       </button>
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        onClick={() => KnowledgeHorrorNav("horror")}
+        onClick={() => Humor('/Quiz?category=horror','horror')}
       >
         괴담
       </button>
