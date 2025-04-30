52f7c9f2b74dbb39d509216a79e5ab465eca5964
김민지 <twoji3019@naver.com>
2025-04-30 12:25:43 +0900

    상태 + 클릭이벤트

--- client/src/components/LoginInput.jsx
+++ client/src/components/LoginInput.jsx
@@ -1,17 +1,25 @@
 import Button from "./Button";
+import {useRef, useState} from 'react'
 
 function LoginInput(){
   // 아이디 입력
   // 비밀번호 입력
   // 로그인 버튼
+  const [userId, setUserId] = useState('')
+  const [pw,setPw] = useState('')
+
+  const [isMatch, setIsMatch] = useState('')
+  const login = ()=>{
+
+  }
 
   return(
     <form className="flex flex-col gap-2">
       <input type="text" placeholder="아이디를 입력해주세요"
-        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" maxLength={30} required></input>
+        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" maxLength={30} required value={userId} onChange={(e)=>setUserId(e.target.value)}></input>
       <input type='password' placeholder="비밀번호를 입력해주세요"
-        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" required></input>
-      <Button text='로그인' colorClass = "bg-gray-300"/>
+        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" required value={pw} onChange={(e)=>setPw(e.target.value)}></input>
+      <Button text='로그인' colorClass = "bg-gray-300" clickEvent={login}/>
     </form>
   )
 }
