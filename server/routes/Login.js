e6ff0e10f01f4e00c71f7488abaceca985a43cd7
김민지 <twoji3019@naver.com>
2025-04-30 11:31:44 +0900

    라우터 뼈대 작성

---
+++ server/routes/Login.js
@@ -0,0 +1,15 @@
+//user_id,password 
+
+import express from 'express'
+import pool from '../config/database.js'
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+
+})
+
+router.post('/',(req,res)=>{
+
+})
+
+export default router
