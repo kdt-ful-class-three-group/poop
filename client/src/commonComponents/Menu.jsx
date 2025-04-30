aef9240caf95c3c0eae950294dc59fec0f70b742
윤종환 <yunjonghwan99@gmail.com>
2025-04-30 14:40:46 +0900

    퀴즈, 상식, 괴담 페이지를 하나의 페이지에서 동작 설정

--- client/src/commonComponents/Menu.jsx
+++ client/src/commonComponents/Menu.jsx
@@ -9,23 +9,40 @@ function Menu() {
     // navText(category);
     navigate("/KnowledgeHorror", { state: { category } });
   };
+
+  const navHandle = (category) => {
+    if (category === 'quiz') {
+      navigate('/Quiz2', {
+        state: { category }
+      })
+    } else if (category === 'knowledge') {
+      navigate('/Quiz2', {
+        state: { category }
+      })
+    } else if (category === 'horror') {
+      navigate('/Quiz2', {
+        state: { category }
+      })
+    }
+  }
+
   return (
     <div className="flex justify-between w-full ">
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        onClick={() => navigate("/Quiz")}
+        onClick={() => navHandle("quiz")}
       >
         퀴즈
       </button>
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400 "
-        onClick={() => KnowledgeHorrorNav("knowledge")}
+        onClick={() => navHandle("knowledge")}
       >
         상식
       </button>
       <button
         className="bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-400"
-        onClick={() => KnowledgeHorrorNav("horror")}
+        onClick={() => navHandle("horror")}
       >
         괴담
       </button>
--- client/src/pages/AllCategory.jsx
+++ client/src/pages/AllCategory.jsx
@@ -2,36 +2,38 @@ import { useEffect, useState } from "react";
 
 import QuizCard from "../components/QuizCard";
 import QuizButton from "../components/QuizButton";
+import { useLocation } from "react-router-dom";
 
 function AllCategory() {
-
-  const [data ,setData] = useState([]);
-  const [num, setNum] = useState(Number(localStorage.getItem('Quiznum'))||0);
+  const location = useLocation();
+  const category = location.state?.category || "없음"; // 기본값 설정
+  const [data, setData] = useState([]);
+  const [num, setNum] = useState(Number(localStorage.getItem(`${category}Num`)) || 0);
 
   //로컬스토리지 - num이 변할 때마다 데이터 저장
-  useEffect(()=>{
-    localStorage.setItem('Quiznum',num.toString())
-  },[num])
+  useEffect(() => {
+    localStorage.setItem(`${category}Num`, num.toString())
+  }, [num])
 
   //fetch 가져오기
-  useEffect(()=>{
-    fetch('http://localhost:8081/quiz')
-    .then((res)=>res.json())
-    .then((data)=>{
-      setData(data);
-    })
+  useEffect(() => {
+    fetch(`http://localhost:8081/${category}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
 
-  },[])
+  }, [category])
 
   //버튼 이벤트
-  const prevBtn = ()=>{
-    if(num>0){
-      setNum(num-1);
+  const prevBtn = () => {
+    if (num > 0) {
+      setNum(num - 1);
     }
   }
-  const nextBtn = ()=>{
-    if(num<data.length-1){
-      setNum(num+1);
+  const nextBtn = () => {
+    if (num < data.length - 1) {
+      setNum(num + 1);
     }
   }
 
@@ -40,8 +42,8 @@ function AllCategory() {
 
   return (
     <div className="w-full">
-      <QuizCard quizData={data[num]}/>
-      <QuizButton nextBtn={()=>nextBtn()} prevBtn={()=>prevBtn()} data={data[num]}/>
+      <QuizCard quizData={data[num]} />
+      <QuizButton nextBtn={() => nextBtn()} prevBtn={() => prevBtn()} data={data[num]} />
     </div>
   );
 }
