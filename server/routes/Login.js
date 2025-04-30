7f80c1c0e1c56ee75407723b4a2d89eb44ce015f
김민지 <twoji3019@naver.com>
2025-04-30 11:50:26 +0900

    데이터가 존재하지 않을 때와 콘솔로 출력 메시지 추가

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -20,11 +20,18 @@ router.get('/:user_id',async(req,res)=>{
   const userId = req.params.user_id
   try{
     const [rows] = await pool.execute('SELECT user_id, password FROM USER WHERE user_id = ? ',[userId])
-    
-    res.json(rows)
+
+    //존재하지 않을 때 - 빈 배열 반환
+    if(rows.length===0){
+      return res.status(404).json({message:'사용자를 찾을 수 없음'})
+    }
+
+    //존재하면 한 명만 조회됨
+    res.json(rows[0])
   }
   catch(err){
-    console.error(err)
+    console.error('DB user 조회 실패',err)
+    res.status(500).json({message:'서버 에러'})
   }
 })
 
