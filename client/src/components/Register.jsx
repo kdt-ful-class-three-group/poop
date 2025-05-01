2d5ccafe3744e7759e0b78d9606b6ba702dc0f43
윤종환 <yunjonghwan99@gmail.com>
2025-05-01 11:38:47 +0900

    이전에는 이메일 컴포넌트를 사용해서 동작하도록 했지만 RegisterMain에서 동작하도록 설정 했기 때문에 필요없어진 상태값을 삭제함

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,34 +1,25 @@
 import Input from "../components/Input.jsx"
-import RegisterEmail from '../components/RegisterEmail';
-import { useState } from 'react'
 
 function Register() {
-  const [showEmail, setShowEmail] = useState(true);
-
   const buttonClick = (e) => {
     setShowEmail(false);
   }
 
   return (
     <div className=" w-full h-full">
-      {showEmail
-        ?
-        (
-          <div className="w-full h-full">
-            <h1 className="justify-start items-left mt-20">회원가입</h1>
-            <div className="h-100 flex flex-col justify-center items-center">
-              <form className="w-full" onSubmit={buttonClick}>
-                <label className="text-sm text-black mb-2 ">아이디</label>
-                <Input type="text" name="username" />
-                <label className="text-sm text-black mb-2">비밀번호</label>
-                <Input type="password" name="password" />
-                <label className="text-sm text-black mb-2">비밀번호 확인</label>
-                <Input type="password" name="passwordCheck" />
-              </form>
-            </div>
-          </div>
-        )
-        : (<RegisterEmail />)}
+      <div className="w-full h-full">
+        <h1 className="justify-start items-left mt-20">회원가입</h1>
+        <div className="h-100 flex flex-col justify-center items-center">
+          <form className="w-full" onSubmit={buttonClick}>
+            <label className="text-sm text-black mb-2 ">아이디</label>
+            <Input type="text" name="username" />
+            <label className="text-sm text-black mb-2">비밀번호</label>
+            <Input type="password" name="password" />
+            <label className="text-sm text-black mb-2">비밀번호 확인</label>
+            <Input type="password" name="passwordCheck" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
