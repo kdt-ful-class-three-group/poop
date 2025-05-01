a505764477f2fac3bb565366dc500ff02a080c2e
김민지 <twoji3019@naver.com>
2025-05-01 11:55:56 +0900

    틀 잡아놓음

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -8,15 +8,24 @@ function Register() {
   const [user, setUser] = useState('')
   //비밀번호
   const [pw, setPw] = useState('')
+  const [checkPw, setCheckPw] = useState('') //비밀번호 확인
 
   //다음화면 보여줄 것인지
   const [showEmail, setShowEmail] = useState(true);
 
   //제출 -> 아이디, 비밀번호 값 담기
-  const changeInput = ()=>{
+  const userCheck = (e)=>{
     // 아이디 특수문자 안됨
 
+  }
+  
+  const pwCheck = (e)=>{
     // 비밀번호 조건 충족
+    
+  }
+
+  const samePwCheck =(e)=>{
+
   }
 
 
@@ -35,11 +44,11 @@ function Register() {
             <div className="h-100 flex flex-col justify-center items-center">
               <form className="w-full" onSubmit={buttonClick}>
                 <label className="text-sm text-black mb-2 ">아이디</label>
-                <Input type="text" name="username" />
+                <Input type="text" name="username" value={user} changeEvent={(e)=>userCheck(e)}/>
                 <label className="text-sm text-black mb-2">비밀번호</label>
-                <Input type="password" name="password" />
+                <Input type="password" name="password" value={pw} changeEvent={(e)=>pwCheck(e)} />
                 <label className="text-sm text-black mb-2">비밀번호 확인</label>
-                <Input type="password" name="passwordCheck" />
+                <Input type="password" name="passwordCheck" value={checkPw} changeEvent={(e)=>samePwCheck(e)} />
                 <button type="submit"
                   className="w-full h-8 text-center text-sm bg-gray-300 hover:bg-gray-500 py-2 rounded mt-4">
                   다음
