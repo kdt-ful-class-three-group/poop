08e5276923cfbcf4ebe27c3842d5376a629fd09d
김민지 <twoji3019@naver.com>
2025-05-01 15:37:15 +0900

    pages의 내용을 component의 Terms로 가져옴

--- client/src/components/Terms.jsx
+++ client/src/components/Terms.jsx
@@ -1,4 +1,45 @@
-function Terms(){
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+function Terms() {
+
+  //전체동의와 관련된 상태
+  const [all, setAll] = useState(false)
+  const [personal, setPersonal] = useState(false)
+  const [terms, setTerms] = useState(false)
+
+  //버튼 상태
+  const [disabled, setDisabled] = useState(true)
+
+  //전체동의 선택과 관련된 실행
+  useEffect(()=>{
+    if(all){
+      setPersonal(all)
+      setTerms(all)
+      setDisabled(!all)
+    }
+  },[all])
+
+  //개별동의에 따른 전첵동의도 변경됨
+  //개별동의 다 체크되면 전체동의 체크
+  //개별동의 체크 하나만 헤제해도 전체동의 해제
+  useEffect(()=>{
+    if(personal&&terms){
+      setAll(true)
+    }
+    if(!personal || !terms){
+      setAll(false)
+    }
+  },[personal,terms])
+
+  //버튼에 따른 이동
+  useEffect(()=>{
+    if(!disabled){
+      // console.log('이동')
+      
+    }
+  },[disabled])
+
   return (
     <div>
       <div className="text-xl">
@@ -6,19 +47,19 @@ function Terms(){
       </div>
       <div className="mt-6 mb-1">
         <label className="flex items-center mb-3">
-          <input className="mr-2" type="checkbox" />
+          <input className="mr-2" type="checkbox" checked={all} onChange={()=>{setAll(!all)}}/>
           <p className="text-xs">약관 전체 동의 하기</p>
         </label>
         <div className="flex items-center justify-between mb-3">
           <label className="flex items-center">
-            <input className="mr-2" type="checkbox" />
+            <input className="mr-2" type="checkbox" checked={personal} onChange={()=>setPersonal(!personal)}/>
             <p className="text-xs">개인정보 수집 및 이용 동의 (필수)</p>
           </label>
           <button className="text-xs">자세히</button>
         </div>
         <div className="flex items-center justify-between mb-3">
           <label className="flex items-center">
-            <input className="mr-2" type="checkbox" />
+            <input className="mr-2" type="checkbox" checked={terms} onChange={()=>setTerms(!terms)}/>
             <p className="text-xs">이용약관 동의 (필수)</p>
           </label>
           <button className="text-xs">자세히</button>
@@ -31,7 +72,7 @@ function Terms(){
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Terms;
