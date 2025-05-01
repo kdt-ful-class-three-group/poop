7a26a329e0ceca4cb12d38ca82b81ea12e3676fa
김민지 <twoji3019@naver.com>
2025-05-01 15:56:18 +0900

    버튼 컴포넌트에 props 속성 추가

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,7 @@
-function Button({text, colorClass, clickEvent}){
+function Button({text, colorClass, clickEvent, disabled}){
 
   return(
-    <button className={`${colorClass} hover:bg-gray-400 py-2 px-4 rounded-md cursor-pointer`} onClick={clickEvent || null}> {text} </button>
+    <button className={`${colorClass} hover:bg-gray-400 py-2 px-4 rounded-md cursor-pointer`} onClick={clickEvent || null} disabled={disabled || false}> {text} </button>
   )
 }
 
