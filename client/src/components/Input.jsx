f4f86f35195eb1a1a4ad4cbfb0118babfb67d539
김민지 <twoji3019@naver.com>
2025-05-01 11:51:31 +0900

    input의 props 추가

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,8 +1,8 @@
-function Input({type, placeholder,name}) {
+function Input({type, placeholder,name, value, changeEvent}) {
 
     return(
         <div>
-            <input className="bg-gray-300 h-10 w-full border-solid mb-8 p-2  "  autoFocus type={type} name={name} required  />
+            <input className="bg-gray-300 h-10 w-full border-solid mb-8 p-2  "  autoFocus type={type} name={name} value={value} onChange={changeEvent} required />
         </div>
     )
 }
