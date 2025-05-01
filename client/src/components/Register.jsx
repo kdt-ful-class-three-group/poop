8d833fa223b1c128ea63af4793e54c00d9fddee0
윤종환 <yunjonghwan99@gmail.com>
2025-05-01 12:29:05 +0900

    회원가입 페이지에서 불필요한 제목 요소를 제거함

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -4,7 +4,6 @@ function Register() {
   return (
     <div className=" w-full h-full">
       <div className="w-full h-full">
-        <h1 className="justify-start items-left mt-20">회원가입</h1>
         <div className="h-100 flex flex-col justify-center items-center">
           <div className="w-full">
             <label className="text-sm text-black mb-2 ">아이디</label>
