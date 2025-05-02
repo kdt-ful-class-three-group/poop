565a5b4228101de442bee0e2781ff72d25ef0fdd
윤종환 <yunjonghwan99@gmail.com>
2025-05-02 17:47:40 +0900

    로그인과 로그아웃이 진행되는 함수를 전달 하도록 설정

--- client/src/context/loginContext.jsx
+++ client/src/context/loginContext.jsx
@@ -1,12 +1,22 @@
-import React, { createContext,useState } from "react";
+import React, { createContext, useState } from "react";
 
 export const LoginContext = createContext();
 
-export const LoginProvider = ({children}) => {
+export const LoginProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
 
+  const logoutHandle = () => {
+    setIsLogin(false);
+  }
+
+  const loginHandle = () => {
+    console.log("제대로 실행이 완료되었다.");
+    setIsLogin(true);
+    console.log("로그인 상태", isLogin);
+  }
+
   return (
-    <LoginContext.Provider value={{isLogin, setIsLogin}}>
+    <LoginContext.Provider value={{ isLogin, loginHandle, loginHandle }}>
       {children}
     </LoginContext.Provider>
   )
