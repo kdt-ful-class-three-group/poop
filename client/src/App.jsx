1a710302f67a28ed37ce4c6caaa5ce98b0f2cd1f
윤종환 <yunjonghwan99@gmail.com>
2025-05-02 15:27:33 +0900

    App.jsx에서 컴포넌트 사용하기 위해서 위치를 최상단으로 이동함

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,33 +16,33 @@ import Nav from "./commonComponents/Nav.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Home>
-          <Routes>
-            {/* Layout 없이 렌더링할 페이지들 */}
-            {/* <Route path="/" element={<Home />} /> */}
-            <LoginProvider>
+    <LoginProvider>
+      <div className="App">
+        <Router>
+          <Home>
+            <Routes>
+              {/* Layout 없이 렌더링할 페이지들 */}
+              {/* <Route path="/" element={<Home />} /> */}
               <Route path="/Community" element={<Community />} />
-            </LoginProvider>
-            <Route
-              path="/Community/CommunityDetail"
-              element={<CommunityDetail />}
-            />
-            <Route path="/Community/write" element={<Write />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Mypage" element={<Mypage />} />
-            <Route path="/Admin" element={<Admin />} />
-            <Route path="/Register" element={<RegisterMain />} />
-            {/* Layout 적용 구간: path 가 "/KnowledgeHorror" 또는 "/Quiz" 인 경우에만 */}
-            <Route element={<Layout />}>
-              <Route path="/Quiz" element={<Quiz />}/>
-            </Route>
-          </Routes>
-        </Home>
-        <Nav />
-      </Router>
-    </div>
+              <Route
+                path="/Community/CommunityDetail"
+                element={<CommunityDetail />}
+              />
+              <Route path="/Community/write" element={<Write />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Mypage" element={<Mypage />} />
+              <Route path="/Admin" element={<Admin />} />
+              <Route path="/Register" element={<RegisterMain />} />
+              {/* Layout 적용 구간: path 가 "/KnowledgeHorror" 또는 "/Quiz" 인 경우에만 */}
+              <Route element={<Layout />}>
+                <Route path="/Quiz" element={<Quiz />} />
+              </Route>
+            </Routes>
+          </Home>
+          <Nav />
+        </Router>
+      </div>
+    </LoginProvider>
   );
 }
 
