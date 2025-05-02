0ecb6ccae1f333e9f62d9808ea6c7ffc13b5f16f
윤종환 <yunjonghwan99@gmail.com>
2025-05-02 15:24:30 +0900

    로그인 컨텍스트 및 제공자 컴포넌트 추가

---
+++ client/src/context/loginContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+const LoginContext = createContext();
+
+export const LoginProvider = ({children}) => {
+  const test = "test";
+
+  return (
+    <loginContext.Provider value={{test}}>
+      {children}
+    </loginContext.Provider>
+  )
+}
