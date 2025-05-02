b99b9d81216c194dd1b0327c760327db0594a920
김민지 <twoji3019@naver.com>
2025-05-02 16:55:53 +0900

    nodemailer 설정

--- server/server.js
+++ server/server.js
@@ -25,7 +25,11 @@ app.use(session({
 }))
 
 const transporter = nodemailer.createTransport({
-  
+  service: process.env.EMAIL_SERVICE,
+  auth:{
+    user : process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
 })
 
 const app = express();
