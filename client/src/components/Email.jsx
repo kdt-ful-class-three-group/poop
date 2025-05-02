086c3409e88f5537c3ca91ef722f35ed9f907cda
김민지 <twoji3019@naver.com>
2025-05-02 15:33:26 +0900

    이메일 형식 확인

--- client/src/components/Email.jsx
+++ client/src/components/Email.jsx
@@ -1,21 +1,42 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import Button from "./Button";
 import { RegisterContext } from '../context/RegisterContext';
 
 function Email({setFlag}){
 
   //이메일 입력값
-  const [email, setEmail] = useEffect('')
+  const [email, setEmail] = useState('')
   //이메일 관련 텍스트
-  const [emailText, setEmailText] = useEffect('')
+  const [emailText, setEmailText] = useState('')
   //이메일 인증 값
-  const [check,setCheck] = useEffect(null)
+  const [check,setCheck] = useState(null)
   //인증 관련 텍스트
-  const [checkText, setCheckText] = useEffect('')
+  const [checkText, setCheckText] = useState('')
   //창고에 데이터 저장
   const {updateData} = useContext(RegisterContext)
   //버튼에 적용
-  const [disabled, setDisabled] = useState(false)
+  const [disabled, setDisabled] = useState(true)
+  //인증 버튼
+  const [isUnCheck, setIsUnCheck] = useState(true)
+
+
+  //이메일 형식 체크
+  const rightEmail =(e)=>{
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //입력창 활성화
+    setEmail(e.target.value)
+
+    const isCorrect =  pattern.test(e.target.value)
+
+
+    if(!isCorrect){
+      setEmailText('이메일 형식과 일치하지 않습니다')
+      setIsUnCheck(true)
+    } else {
+      setEmailText('')
+      setIsUnCheck(false)
+    }
+  }
 
   return(
     <div className="flex flex-col gap-1 w-full">
@@ -23,10 +44,10 @@ function Email({setFlag}){
       <form className="flex flex-col gap-1">
         <p className="text-sm">이메일</p>
         <div className="flex justify-between">
-          <input type="text" className="w-9/12 bg-gray-300 focus:bg-gray-100" />
-          <Button text='인증' colorClass={'bg-gray-300'}/>
+          <input type="text" className="w-9/12 bg-gray-300 focus:bg-gray-100" onChange={(e)=>rightEmail(e)} value={email}/>
+          <Button text='인증' colorClass={'bg-gray-300'} disabled={isUnCheck}/>
         </div>
-        <p className="text-xs text-red-500">이메일 형식과 일치하지 않습니다</p>
+        <p className="text-xs text-red-500">{emailText}</p>
       </form>
       {/* 이메일 인증 */}
       <div className="flex flex-col gap-1">
