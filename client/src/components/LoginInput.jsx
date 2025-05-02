341425503694e4a726ce78ceb5bf86282d42d2f4
윤종환 <yunjonghwan99@gmail.com>
2025-05-02 17:48:08 +0900

    로그인이 성공했을 경우 loginHandle이 실행 되도록 설정을 해줌

--- client/src/components/LoginInput.jsx
+++ client/src/components/LoginInput.jsx
@@ -1,55 +1,55 @@
 import Button from "./Button";
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../context/loginContext";
 
-function LoginInput({autoLogin}){
+function LoginInput({ autoLogin }) {
 
   const navigate = useNavigate()
   // 아이디 입력
   // 비밀번호 입력
   // 로그인 버튼
   const [userId, setUserId] = useState('')
-  const [pw,setPw] = useState('')
+  const [pw, setPw] = useState('')
+  const [text, setText] = useState('')
+  const { loginHandle } = useContext(LoginContext);
 
-  const [text,setText]= useState('')
-
-
-  const login = async()=>{
-
-    const response = await fetch('http://localhost:8080/login',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json',
-        'Accept':'application/json'
+  const login = async () => {
+    const response = await fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       },
-      credentials:'include',
-      body:JSON.stringify({user_id:userId,password:pw,autoLogin:autoLogin})
+      credentials: 'include',
+      body: JSON.stringify({ user_id: userId, password: pw, autoLogin: autoLogin })
     })
 
     const result = await response.json()
-    
+
     console.log(result)
-    if(result.success){
+    if (result.success) {
       setText('')
       console.log(result.success)
       //성공하면 sessionStorage에 저장
-      sessionStorage.setItem('user',result.user.user_id)
+      sessionStorage.setItem('user', result.user.user_id)
       // 성공하면 퀴즈로 이동
-      navigate('/quiz')
-    } else{
+      navigate('/quiz');
+      loginHandle;
+    } else {
       setText(result.message)
       console.log(result.success)
     }
   }
 
-  return(
-    <form className="flex flex-col gap-2" onSubmit={(e)=>e.preventDefault()}>
+  return (
+    <form className="flex flex-col gap-2" onSubmit={(e) => e.preventDefault()}>
       <input type="text" placeholder="아이디를 입력해주세요"
-        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" maxLength={30} required value={userId} onChange={(e)=>setUserId(e.target.value)}></input>
+        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" maxLength={30} required value={userId} onChange={(e) => setUserId(e.target.value)}></input>
       <input type='password' placeholder="비밀번호를 입력해주세요"
-        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" required value={pw} onChange={(e)=>setPw(e.target.value)}></input>
+        className="bg-gray-300 px-4 py-2 rounded-md focus:bg-gray-100" required value={pw} onChange={(e) => setPw(e.target.value)}></input>
       <p className="text-red-500">{text}</p>
-      <Button text='로그인' colorClass = "bg-gray-300" clickEvent={()=>login()}/>
+      <Button text='로그인' colorClass="bg-gray-300" clickEvent={() => login()} />
     </form>
   )
 }
