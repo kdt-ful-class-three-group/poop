2c72d129f4b9057231554b6e5a9176d38ffc3ce8
김민지 <muunji@github.com>
2025-05-09 16:28:34 +0900

    수정 버튼 작성

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import QuilEditor from "../components/QuilEditor.jsx";
 import { useEffect, useState } from "react";
 
@@ -9,6 +9,32 @@ function Write() {
     //입력될 내용
     const location = useLocation()
     const [data,setData]= useState(location.state || {})
+    // 작성 완료 -> 상세페이지로 이동
+    const navigate = useNavigate()
+
+    //수정 클릭 이벤트
+    const editBtn = async(e)=>{
+        e.preventDefault()
+
+        console.log('수정 요청',data.board_id)
+
+        //fetch
+        const response = await fetch(`http://localhost:8080/community/update/${data.board_id}`,{
+            method:'PUT',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({title:data.title, content:data.content,id:page})
+        })
+
+        if(response.ok){
+            navigate(`/community/communityDetail/${page}`)
+        }else {
+            alert('수정 실패했습니다')
+        }
+
+
+    }
 
 
     return (
@@ -32,6 +58,7 @@ function Write() {
                     <button
                         className="mt-10 w-full bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
                         type="submit"
+                        onClick={(e)=>editBtn(e)}
                     >
                         등록하기
                     </button>
