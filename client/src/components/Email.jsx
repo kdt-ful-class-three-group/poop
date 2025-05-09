3b177b6e406e0f8c37ca2f19d91ba18c7176198d
윤종환 <yunjonghwan99@gmail.com>
2025-05-09 15:18:50 +0900

    Email 컴포넌트에서 인증 코드 확인 시 이메일을 updateFormData로 업데이트하도록 수정

--- client/src/components/Email.jsx
+++ client/src/components/Email.jsx
@@ -1,17 +1,18 @@
 // src/components/Email.jsx
 import { useState, useEffect } from "react";
+import { userRegister } from "../context/RegisterContext";
 
-function Email({nextHandle}) {
+function Email({ nextHandle }) {
   const [email, setemail] = useState('');
   const [isEmailVaild, setIsEmailVaild] = useState(false);
   const [showEmailError, setShowEmailError] = useState(false);
   const [authCode, setAuthCode] = useState(''); // 사용자가 입력하는 인증번호
   const [sentCode, setSentCode] = useState(''); // 서버에서 보낸 인증번호
   const [isCodeMatch, setIsCodeMatch] = useState(null); // null: 아직 입력 안 함, true/false
-
+  const { updateFormData } = userRegister();
   // 이메일 정규식 검사
   const validateEmail = (email) => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   };
 
   useEffect(() => {
@@ -59,31 +60,32 @@ function Email({nextHandle}) {
       }
     } else {
       setIsCodeMatch(null);
-    }  
+    }
   };
 
   const handleNext = () => {
     if (isCodeMatch && nextHandle) {
+      updateFormData("email", email);
       nextHandle(); // 다음 단계로 이동
     }
   }
 
 
-  return(
-    <div className="flex flex-col gap-1 w-full"> 
+  return (
+    <div className="flex flex-col gap-1 w-full">
       {/* 이메일 */}
       <form className="flex flex-col gap-1">
         <p className="text-sm">이메일</p>
         <div className="flex justify-between">
-          
+
           <input type="text" className="w-9/12 bg-gray-300 focus:bg-gray-100"
-          value={email}
-          onChange={(e) => setemail(e.target.value)}/>
+            value={email}
+            onChange={(e) => setemail(e.target.value)} />
           <button
-          type="button"
-          onClick={handleSendCode}
-          disabled={!isEmailVaild}
-          className={`px-4 py-2 rounded ${isEmailVaild ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-500"}`}
+            type="button"
+            onClick={handleSendCode}
+            disabled={!isEmailVaild}
+            className={`px-4 py-2 rounded ${isEmailVaild ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-500"}`}
           >
             인증
           </button>
@@ -96,8 +98,8 @@ function Email({nextHandle}) {
       <div className="flex flex-col gap-1">
         <p className="text-sm" >이메일 인증</p>
         <input type='text' className="w-full bg-gray-300 py-2 focus:bg-gray-100"
-        value={authCode}
-        onChange={handleCheckCode}
+          value={authCode}
+          onChange={handleCheckCode}
         />
         {isCodeMatch === true && (
           <p className="text-xs text-green-500">인증번호가 일치합니다</p>
@@ -107,14 +109,14 @@ function Email({nextHandle}) {
         )}
       </div>
 
-        <button
+      <button
         type="button"
         disabled={!isCodeMatch}
         onClick={handleNext}
         className={`w-full py-2 rounded ${isCodeMatch ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-500"}`}
-        >
-          다음
-        </button>
+      >
+        다음
+      </button>
 
     </div>
   )
