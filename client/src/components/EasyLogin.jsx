57a7e1db8c5750138329e5fa267c6de5f9e7ef34
JadenMeister <bigdragon00@naver.com>
2025-05-09 10:38:01 +0900

    feature: 소셜로그인 추가 테스트

--- client/src/components/EasyLogin.jsx
+++ client/src/components/EasyLogin.jsx
@@ -1,22 +1,20 @@
 import Button from "./Button";
+import{KakaoBtn, GoogleBtn, NaverBtn} from "oauth-btn";
 
 function EasyLogin(){
 
   const ButtonTexts= [
-    "카카오톡으로 계속하기",
-    "네이버로 계속하기",
-    "구글로 계속하기"
+
   ]
 
   return(
-    <div className="flex flex-col gap-2">
-      {ButtonTexts.map((text,i)=>(
-        <Button
-          key={i}
-          text={text}
-          colorClass='bg-gray-300'></Button>
-      ))}
-    </div>
+
+    <>
+      <KakaoBtn shape="rect"  />
+      <NaverBtn shape="rect" />
+      <GoogleBtn className="border-2 border-black w-50 " shape="rect" />
+    </>
+
   )
 }
 
