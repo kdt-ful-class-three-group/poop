45a8edf1b276424fea7b0bb7316dc2469c3aadfd
김재승 <jaes2878@gmail.com>
2025-05-09 10:26:44 +0900

    input컴포넌트 설명 주석 추가

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const forbiddenPattern = /[`<>\/]|script|onerror|onload|alert|<|>|\\/gi;
+const forbiddenPattern = /[`<>\/]|script|onerror|onload|alert|<|>|\\/gi; //* 정규식 패턴
 function Input({ className, type, placeholder, name, value, onChange }) {
   const [inputPattern, setInputPattern] = useState(true);
   const inputForbiddenPattern = (e) => {
@@ -7,12 +7,12 @@ function Input({ className, type, placeholder, name, value, onChange }) {
     const hasInputForbiddenPattern = forbiddenPattern.test(inputValue);
     if (hasInputForbiddenPattern) {
       setInputPattern(false);
-      inputValue = inputValue.replace(forbiddenPattern, "");
+      inputValue = inputValue.replace(forbiddenPattern, ""); //* 정규식 패턴 입력 시 value값을 지워줌
       // return;
     } else {
       setInputPattern(true);
     }
-    onChange({ target: { name, value: inputValue } });
+    onChange({ target: { name, value: inputValue } }); //* input에 name값과 value값을 넣어줌
   };
   return (
     <div>
