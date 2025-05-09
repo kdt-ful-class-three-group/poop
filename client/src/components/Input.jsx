2ff0a5055d8b2bb32942e1e95be3bbf3e91966d1
윤종환 <yunjonghwan99@gmail.com>
2025-05-09 10:40:40 +0900

    Input 컴포넌트에 입력값 검증 로직 추가 및 불법 패턴 제거 기능 구현

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+const forbiddenPattern = /[`<>\/]|script|onerror|onload|alert|<|>|\\/gi; //* 정규식 패턴
 function Input({ className, type, placeholder, name, value, onChange }) {
+  const [inputPattern, setInputPattern] = useState(true);
+  const inputForbiddenPattern = (e) => {
+    let inputValue = e.target.value;
+    const hasInputForbiddenPattern = forbiddenPattern.test(inputValue);
+    if (hasInputForbiddenPattern) {
+      setInputPattern(false);
+      inputValue = inputValue.replace(forbiddenPattern, ""); //* 정규식 패턴 입력 시 value값을 지워줌
+      // return;
+    } else {
+      setInputPattern(true);
+    }
+    onChange({ target: { name, value: inputValue } }); //* input에 name값과 value값을 넣어줌
+  };
   return (
     <div>
       <input
@@ -7,7 +22,8 @@ function Input({ className, type, placeholder, name, value, onChange }) {
         type={type}
         name={name}
         value={value}
-        onChange={onChange}
+        placeholder={placeholder}
+        onChange={inputForbiddenPattern}
         required
       />
     </div>
