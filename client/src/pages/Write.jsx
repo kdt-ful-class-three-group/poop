9469095f1242ab409d878766df13e7a2f8eebfa8
김민지 <muunji@github.com>
2025-05-09 16:09:57 +0900

    수정 링크로 이동 > 입력창에 값 띄우기 시도 중

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,6 +1,16 @@
+import { useLocation, useParams } from "react-router-dom";
 import QuilEditor from "../components/QuilEditor.jsx";
+import { useEffect, useState } from "react";
 
 function Write() {
+    //링크에 숫자가 있으면
+    const params = useParams()
+    const [page, setPage] = useState(params.board_id || null)
+    //입력될 내용
+    const location = useLocation()
+    const [data,setData]= useState(location.state || {})
+
+
     return (
         <div className="w-full min-h-screen flex flex-col">
             <h1 className="mt-5 font-bold">게시글 작성</h1>
@@ -15,8 +25,9 @@ function Write() {
                             name="title"
                             required
                             placeholder="제목"
+                            value={params ? data.title : '제목'}
                         />
-                        <QuilEditor />
+                        <QuilEditor value={params ? data.content : null}/>
                     </div>
                     <button
                         className="mt-10 w-full bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
