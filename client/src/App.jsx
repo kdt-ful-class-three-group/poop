7e79bb55c1a857d388b0d1be4a7f95746f3e661f
JadenMeister <bigdragon00@naver.com>
2025-05-12 12:35:18 +0900

    feat: swagger Test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Quiz from "./pages/Quiz.jsx";
 import Mypage from "./pages/Mypage.jsx";
-import Admin from "./pages/Admin.jsx";
+
 import Layout from "./layout/Layout.jsx";
 import Write from "./pages/Write.jsx";
 import RegisterMain from "./pages/RegisterMain.jsx";
@@ -22,7 +22,7 @@ function App() {
           <Home>
             <Routes>
               {/* Layout 없이 렌더링할 페이지들 */}
-              {/* <Route path="/" element={<Home />} /> */}
+               <Route path="/" element={<Quiz />} />
               <Route path="/Community" element={<Community />} />
               <Route
                 path="/Community/CommunityDetail/:board_id"
@@ -33,7 +33,6 @@ function App() {
               <Route path="/Community/write/:board_id" element={<Write />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/Mypage" element={<Mypage />} />
-              <Route path="/Admin" element={<Admin />} />
               <Route path="/Register" element={<RegisterMain />} />
               {/* Layout 적용 구간: path 가 "/KnowledgeHorror" 또는 "/Quiz" 인 경우에만 */}
               <Route element={<Layout />}>
--- server/config/database.js
+++ server/config/database.js
@@ -2,11 +2,11 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log('🚨 DB ENV:', process.env.DB_HOST, process.env.DB_PORT, process.env.DB_USER, process.env.DB_PASSWORD ? '(ok)' : '(없음)', process.env.DB_NAME);
+
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT), // 꼭 숫자로 변환
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
