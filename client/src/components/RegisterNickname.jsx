e98c5cc3290b664f17f73f0664a6abd5a7bac142
김재승 <jaes2878@gmail.com>
2025-05-12 16:51:36 +0900

    이메일 라우터 env수정

--- client/src/components/RegisterNickname.jsx
+++ client/src/components/RegisterNickname.jsx
@@ -1,21 +1,16 @@
-
 import React from "react";
 import { fetchRegister } from "../api/fectchApi.js";
 import { useNavigate } from "react-router-dom";
 import { userRegister } from "../context/RegisterContext.jsx";
 
-
 const RegisterNickname = () => {
-
   const navigate = useNavigate();
   const { formData, updateFormData } = userRegister();
-  console.log("현재까지 받은 유저 정보", formData)
+  console.log("현재까지 받은 유저 정보", formData);
 
   const [nickname, setNickname] = React.useState("");
 
   const handelSubmit = async (e) => {
-
-
     if (!nickname || nickname.trim() === "") {
       alert("닉네임을 입력해주세요.");
       return;
@@ -29,8 +24,8 @@ const RegisterNickname = () => {
       return;
     }
     const updated = {
-      user_nick: nickname
-    }
+      user_nick: nickname,
+    };
     updateFormData("user_nick", nickname);
     console.log("닉네임을 받은 유저 정보", updated);
     console.log("최종 유저 정보", formData);
@@ -40,14 +35,10 @@ const RegisterNickname = () => {
       console.log("유저 등록 성공", userData);
       alert("회원가입이 완료되었습니다.");
       navigate("/login");
-
     } catch (err) {
       console.error("유저 등록 에러", err);
     }
-
-  }
-
-
+  };
 
   return (
     <div className="w-full ">
@@ -56,9 +47,9 @@ const RegisterNickname = () => {
         <input
           type="text"
           name="nickname"
+          value={nickname}
           onChange={(e) => setNickname(e.target.value)}
           placeholder="닉네임을 입력해 주세요"
-          value={nickname}
           maxLength="8"
           className="w-full p-2 border rounded bg-gray-200"
         />
@@ -66,7 +57,15 @@ const RegisterNickname = () => {
 
       {/* 3. 다음 버튼 */}
       <div>
-        <button onClick={handelSubmit} type="submit" className={`w-full py-2 rounded ${nickname === "" ? "bg-gray-300" : "bg-blue-500"}`}>다음</button>
+        <button
+          onClick={handelSubmit}
+          type="submit"
+          className={`w-full py-2 rounded ${
+            nickname === "" ? "bg-gray-300" : "bg-blue-500"
+          }`}
+        >
+          다음
+        </button>
       </div>
     </div>
   );
--- server/routes/email.js
+++ server/routes/email.js
@@ -4,8 +4,6 @@ import nodemailer from "nodemailer";
 
 const router = express.Router();
 
-
-
 router.post("/send", async (req, res) => {
   const { email } = req.body;
 
@@ -14,25 +12,23 @@ router.post("/send", async (req, res) => {
   req.session.verificationCode = code; // 세션에 저장
   req.session.email = email; // 세션에 이메일 저장
 
-
   const transporter = nodemailer.createTransport({
     service: "Gmail", // 또는 다른 SMTP
     auth: {
-      user: process.env.EMAIL_USER, // .env에서 불러오는 이메일 주소
-      pass: process.env.EMAIL_PASS, // 앱 비밀번호 or 실제 비밀번호
+      user: process.env.EMAIL_ID, // .env에서 불러오는 이메일 주소
+      pass: process.env.EMAIL_PW, // 앱 비밀번호 or 실제 비밀번호
     },
   });
 
   const mailOptions = {
-    from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+    from: `"Your App Name" <${process.env.EMAIL_ID}>`,
     to: email,
     subject: "회원가입 이메일 인증 코드",
     text: `인증 코드는 ${code} 입니다.`,
-  }
-
+  };
 
   try {
-    await transporter.sendMail(mailOptions)
+    await transporter.sendMail(mailOptions);
     res.status(200).json({ success: true, code: code });
   } catch (error) {
     console.error(error);
