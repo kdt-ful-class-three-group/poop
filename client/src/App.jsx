eb75c6d3c2c9ff6bef215b820032d6796b289ccc
윤종환 <yunjonghwan99@gmail.com>
2025-05-12 11:47:25 +0900

    feat: Admin 페이지 라우터 삭제

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Quiz from "./pages/Quiz.jsx";
 import Mypage from "./pages/Mypage.jsx";
-import Admin from "./pages/Admin.jsx";
 import Layout from "./layout/Layout.jsx";
 import Write from "./pages/Write.jsx";
 import RegisterMain from "./pages/RegisterMain.jsx";
@@ -33,7 +32,6 @@ function App() {
               <Route path="/Community/write/:board_id" element={<Write />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/Mypage" element={<Mypage />} />
-              <Route path="/Admin" element={<Admin />} />
               <Route path="/Register" element={<RegisterMain />} />
               {/* Layout 적용 구간: path 가 "/KnowledgeHorror" 또는 "/Quiz" 인 경우에만 */}
               <Route element={<Layout />}>
