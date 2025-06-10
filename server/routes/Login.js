aca771df15480e0a716166d1d081fa2f30121904
윤종환 <yunjonghwan99@gmail.com>
2025-06-10 21:45:43 +0900

    fix: 쿼리에서 사용자 테이블 이름을 수정하고 데이터 반환 방식을 일관성 있게 변경

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -9,8 +9,8 @@ const router = express.Router()
 //모든 데이터조회
 router.get('/', async (req, res) => {
   try {
-    const [rows] = await pool.execute('SELECT user_id,password FROM user')
-    res.json(rows)
+    const result = await pool.query('SELECT user_id,password FROM users')
+    res.json(result.rows)
   }
   catch (err) {
     console.error(err)
@@ -21,15 +21,15 @@ router.get('/', async (req, res) => {
 router.get('/:user_id', async (req, res) => {
   const userId = req.params.user_id
   try {
-    const [rows] = await pool.execute('SELECT user_id, password FROM user WHERE user_id = ? ', [userId])
+    const result = await pool.query('SELECT user_id, password FROM users WHERE user_id = ? ', [userId])
 
     //존재하지 않을 때 - 빈 배열 반환
-    if (rows.length === 0) {
+    if (result.rows.length === 0) {
       return res.status(404).json({ message: '사용자를 찾을 수 없음' })
     }
 
     //존재하면 한 명만 조회됨
-    res.json(rows[0])
+    res.json(result.rows[0])
   }
   catch (err) {
     console.error('DB user 조회 실패', err)
